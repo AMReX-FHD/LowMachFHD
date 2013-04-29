1c74cf28854df49de82264cd8bb748b016410f75
ajnonaka <AJNonaka@lbl.gov>
2013-04-29 13:09:35 -0700

    pulling more code in from the older explicit code.

--- exec/lowMach_implicit/initial_projection.f90
+++ exec/lowMach_implicit/initial_projection.f90
@@ -2,6 +2,11 @@ module initial_projection_module
 
   use multifab_module
   use ml_layout_module
+  use convert_stag_module
+  use convert_variables_module
+  use define_bc_module
+  use macproject_module
+  use div_and_grad_module
 
   implicit none
 
@@ -11,28 +16,75 @@ module initial_projection_module
 
 contains
 
-  subroutine initial_projection(mla,mold,umac,sold)
+  subroutine initial_projection(mla,mold,umac,sold,dx,the_bc_tower)
 
     type(ml_layout), intent(in   ) :: mla
     type(multifab) , intent(inout) :: mold(:,:)
     type(multifab) , intent(inout) :: umac(:,:)
     type(multifab) , intent(in   ) :: sold(:)
+    real(kind=dp_t), intent(in   ) :: dx(:,:)
+    type(bc_tower) , intent(in   ) :: the_bc_tower
 
     ! local
-    type(multifab) :: rho_edge(mla%nlevel,mla%dim)
+    integer :: i,dm,n,nlevs
 
-    ! create rho on faces
+    type(multifab) ::     mac_rhs(mla%nlevel)
+    type(multifab) ::         phi(mla%nlevel)
+    type(multifab) ::    rho_face(mla%nlevel,mla%dim)
+    type(multifab) :: rhoinv_face(mla%nlevel,mla%dim)
+
+    dm = mla%dim
+    nlevs = mla%nlevel
+
+    do n=1,nlevs
+       call multifab_build(mac_rhs(n),mla%la(n),1,0)
+       call multifab_build(phi(n),mla%la(n),1,1)
+       call setval(phi(n),0.d0)
+       do i=1,dm
+          call multifab_build_edge(rho_face(n,i),mla%la(n),1,0,i)
+          call multifab_build_edge(rhoinv_face(n,i),mla%la(n),1,0,i)
+       end do       
+    end do
+
+    ! create average rho to faces
+    call average_cc_to_face(nlevs,sold,rho_face,1,dm+2,1,the_bc_tower%bc_tower_array)
 
     ! convert m to u
+    call convert_m_to_umac(mla,rho_face,mold,umac,.true.)
 
+    !!!!!!!!!!!!!!!!!!!!!!!!!
     ! build rhs = S - div(u)
+    !!!!!!!!!!!!!!!!!!!!!!!!!
+
+    ! compute mac_rhs = -div(u)
+    call compute_divu(mla,umac,mac_rhs,dx)
+    do n=1,nlevs
+       call multifab_mult_mult_s_c(mac_rhs(n),1,-1.d0,1,0)
+    end do
+
 
     ! project to solve for phi - use the 'full' solver
+    call macproject(mla,phi,umac,sold,mac_rhs,dx,the_bc_tower,.true.)
 
-    ! subtract off pressure gradient
+    ! compute (1/rho)
+    call average_cc_to_face_inv(nlevs,sold,rhoinv_face,1,dm+2,1,the_bc_tower%bc_tower_array)
+
+    ! umac = umac - (1/rho) grad phi
+    call subtract_weighted_gradp(mla,umac,rhoinv_face,phi,dx)
 
     ! convert u to m
+    call convert_m_to_umac(mla,rho_face,mold,umac,.false.)
 
+
+
+    do n=1,nlevs
+       call multifab_destroy(mac_rhs(n))
+       call multifab_destroy(phi(n))
+       do i=1,dm
+          call multifab_destroy(rho_face(n,i))
+          call multifab_destroy(rhoinv_face(n,i))
+       end do
+    end do
 
   end subroutine initial_projection
 
--- src_lowMach/GPackage.mak
+++ src_lowMach/GPackage.mak
@@ -1,5 +1,6 @@
 f90sources += convert_variables.f90
 f90sources += exact_solutions.f90
 f90sources += init.f90
+f90sources += mk_diffusive_fluxdiv.f90
 f90sources += probin_lowMach.f90
 f90sources += write_plotfile.f90
