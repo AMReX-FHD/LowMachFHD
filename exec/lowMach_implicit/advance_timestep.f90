48d8fc27c6daedf3feecdf1e4e15fa08df5525f8
ajnonaka <AJNonaka@lbl.gov>
2013-04-30 10:09:18 -0700

    more corrector work.  WIP

--- exec/lowMach_implicit/advance_timestep.f90
+++ exec/lowMach_implicit/advance_timestep.f90
@@ -42,7 +42,8 @@ contains
     type(multifab) ::      s_face(mla%nlevel,mla%dim)
     type(multifab) :: gmres_rhs_v(mla%nlevel,mla%dim)
     type(multifab) ::    chi_face(mla%nlevel,mla%dim)
-
+    type(multifab) :: m_a_fluxdiv(mla%nlevel,mla%dim)
+    type(multifab) :: m_d_fluxdiv(mla%nlevel,mla%dim)
 
     type(multifab) :: eta_nodal(mla%nlevel)   ! averaged to nodes (2D only)
     type(multifab) ::  eta_edge(mla%nlevel,3) ! averaged to edges (3D only; xy/xz/yz edges)
@@ -63,6 +64,8 @@ contains
           call multifab_build_edge(     s_face(n,i),mla%la(n),nscal,0,i)
           call multifab_build_edge(gmres_rhs_v(n,i),mla%la(n),1,0,i)
           call multifab_build_edge(   chi_face(n,i),mla%la(n),1,0,i)
+          call multifab_build_edge(m_a_fluxdiv(n,i),mla%la(n),1,0,i)
+          call multifab_build_edge(m_d_fluxdiv(n,i),mla%la(n),1,0,i)
        end do
     end do
 
@@ -91,6 +94,10 @@ contains
     do n=1,nlevs
        call setval(s_update(n),0.d0,all=.true.)
        call setval(phi(n)     ,0.d0,all=.true.)
+       do i=1,dm
+          call setval(m_a_fluxdiv(n,i),0.d0,all=.true.)
+          call setval(m_d_fluxdiv(n,i),0.d0,all=.true.)
+       end do
     end do
 
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -111,11 +118,11 @@ contains
     ! compute advective flux divergence
     call mk_advective_s_fluxdiv(mla,umac,s_face,s_update,dx)
 
-    ! compute del dot rho c grad c
+    ! compute del dot rho chi grad c
     call mk_diffusive_rhoc_fluxdiv(mla,s_update,2,prim,s_face,chi_face,dx, &
                                    the_bc_tower%bc_tower_array)
 
-    ! snew = sold + dt * del dot (A + D)
+    ! snew = sold + dt * del dot (A+D)
     do n=1,nlevs
        call saxpy(snew(n),1.d0,sold(n),fixed_dt,s_update(n))
     end do
@@ -128,12 +135,6 @@ contains
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     ! Step 2 - Crank-Nicolson Velocity Predictor
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    ! multiply eta and kappa by dt/2
-    do n=1,nlevs
-       call multifab_mult_mult_s_c(eta(n)  ,1,fixed_dt/2.d0,1,1)
-       call multifab_mult_mult_s_c(kappa(n),1,fixed_dt/2.d0,1,1)
-    end do
 
     ! compute eta on nodes (2D) or edges (3D)
     if (dm .eq. 2) then
@@ -149,12 +150,28 @@ contains
        end do
     end do
 
-    ! build up the rhs_v - add dt/2*D
-    call mk_diffusive_m_fluxdiv(mla,gmres_rhs_v,umac,eta,eta_nodal,eta_edge, &
+    ! compute diffusive flux divergence for m
+    call mk_diffusive_m_fluxdiv(mla,m_d_fluxdiv,umac,eta,eta_nodal,eta_edge, &
                                 kappa,dx,the_bc_tower%bc_tower_array)
 
-    ! build up the rhs_v - add dt*A
-    call mk_advective_m_fluxdiv(mla,umac,mold,gmres_rhs_v,dx)
+    ! multiply by dt/2 and add to rhs_v
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_mult_mult_s_c(m_d_fluxdiv(n,i),1,fixed_dt/2.d0,1,0)
+          call multifab_plus_plus_c(gmres_rhs_v(n,i),1,m_d_fluxdiv(n,i),1,1,0)
+       end do
+    end do
+
+    ! compute advective flux divergence for m
+    call mk_advective_m_fluxdiv(mla,umac,mold,m_a_fluxdiv,dx)
+
+    ! multiply by dt and add to rhs_v
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_mult_mult_s_c(m_a_fluxdiv(n,i),1,fixed_dt,1,0)
+          call multifab_plus_plus_c(gmres_rhs_v(n,i),1,m_a_fluxdiv(n,i),1,1,0)
+       end do
+    end do
 
     ! initialize rhs_p to zero since subsequent subroutines will add to it
     do n=1,nlevs
@@ -165,9 +182,21 @@ contains
     call mk_diffusive_rhoc_fluxdiv(mla,gmres_rhs_p,1,prim,s_face,chi_face,dx, &
                                    the_bc_tower%bc_tower_array)
 
+    ! multiply eta and kappa by dt/2
+    do n=1,nlevs
+       call multifab_mult_mult_s_c(eta(n)  ,1,fixed_dt/2.d0,1,1)
+       call multifab_mult_mult_s_c(kappa(n),1,fixed_dt/2.d0,1,1)
+    end do
+
     ! call gmres to compute umac^{n+1,*}
     call gmres(mla,the_bc_tower,dx,gmres_rhs_v,gmres_rhs_p,umac,phi,snew, &
                eta,kappa,theta_fac)
+
+    ! restore eta and kappa by 2/dt
+    do n=1,nlevs
+       call multifab_mult_mult_s_c(eta(n)  ,1,2.d0/fixed_dt,1,1)
+       call multifab_mult_mult_s_c(kappa(n),1,2.d0/fixed_dt,1,1)
+    end do
 
     ! convert umac to mnew
     call convert_m_to_umac(mla,s_face,mnew,umac,.false.)
@@ -178,33 +207,108 @@ contains
        end do
     end do
 
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Step 3 - Trapezoidal Scalar Corrector
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    ! reset s_update
+    do n=1,nlevs
+       call setval(s_update(n),0.d0,all=.true.)
+    end do
+    
+    ! compute prim from snew
+    call convert_cons_to_prim(mla,snew,prim,.true.)
+
+    ! average snew to faces
+    do i=1,nscal
+       call average_cc_to_face(nlevs,snew,s_face,i,dm+2,1,the_bc_tower%bc_tower_array)
+    end do
+
+    ! compute advective flux divergence
+    call mk_advective_s_fluxdiv(mla,umac,s_face,s_update,dx)
+
+    ! compute del dot rho chi grad c
+    call mk_diffusive_rhoc_fluxdiv(mla,s_update,2,prim,s_face,chi_face,dx, &
+                                   the_bc_tower%bc_tower_array)
+
+    ! snew = (1/2)*snew + (1/2)*sold + (1/2)*del dot (A+D)
+    do n=1,nlevs
+       call multifab_mult_mult_s_c(snew(n),1,0.5d0,nscal,snew(n)%ng)
+       call saxpy(snew(n),1,0.5d0,sold(n))
+       call saxpy(snew(n),1,fixed_dt/2.d0,s_update(n))
+       call multifab_fill_boundary(snew(n))
+    end do
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Step 4 - Crank-Nicolson Velocity Corrector
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    ! build up the rhs_v - set rhs to rho^n v^n
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_copy_c(gmres_rhs_v(n,i),1,mold(n,i),1,1,0)
+       end do
+    end do
+
+    ! add old diffusive flux divergenct to rhs_v
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_plus_plus_c(gmres_rhs_v(n,i),1,m_d_fluxdiv(n,i),1,1,0)
+       end do
+    end do
+
+    ! multiply old advective flux divergence by 1/2 and add to rhs_v
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_mult_mult_s_c(m_a_fluxdiv(n,i),1,0.5d0,1,0)
+          call multifab_plus_plus_c(gmres_rhs_v(n,i),1,m_a_fluxdiv(n,i),1,1,0)
+       end do
+    end do
+
+    ! compute advective flux divergence for m
+    call mk_advective_m_fluxdiv(mla,umac,mnew,m_a_fluxdiv,dx)
+
+    ! multiply by dt/2 and add to rhs_v
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_mult_mult_s_c(m_a_fluxdiv(n,i),1,fixed_dt/2.d0,1,0)
+          call multifab_plus_plus_c(gmres_rhs_v(n,i),1,m_a_fluxdiv(n,i),1,1,0)
+       end do
+    end do
+
+    ! initialize rhs_p to zero since subsequent subroutines will add to it
+    do n=1,nlevs
+       call setval(gmres_rhs_p(n),0.d0,all=.true.)
+    end do
+
+    ! add del dot rho chi grad c to rhs_p
+    call mk_diffusive_rhoc_fluxdiv(mla,gmres_rhs_p,1,prim,s_face,chi_face,dx, &
+                                   the_bc_tower%bc_tower_array)
+
+    ! multiply eta and kappa by dt/2
+    do n=1,nlevs
+       call multifab_mult_mult_s_c(eta(n)  ,1,fixed_dt/2.d0,1,1)
+       call multifab_mult_mult_s_c(kappa(n),1,fixed_dt/2.d0,1,1)
+    end do
+
+    ! call gmres to compute umac^{n+1,*}
+    call gmres(mla,the_bc_tower,dx,gmres_rhs_v,gmres_rhs_p,umac,phi,snew, &
+               eta,kappa,theta_fac)
+
     ! restore eta and kappa by 2/dt
     do n=1,nlevs
        call multifab_mult_mult_s_c(eta(n)  ,1,2.d0/fixed_dt,1,1)
        call multifab_mult_mult_s_c(kappa(n),1,2.d0/fixed_dt,1,1)
     end do
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    ! Step 3 - Trapezoidal Scalar Corrector
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    
-    ! average s to faces
-    do i=1,nscal
-       call average_cc_to_face(nlevs,snew,s_face,i,dm+2,1,the_bc_tower%bc_tower_array)
-    end do
+    ! convert umac to mnew
+    call convert_m_to_umac(mla,s_face,mnew,umac,.false.)
 
-    ! reset s_update
     do n=1,nlevs
-       call setval(s_update(n),0.d0,all=.true.)
+       do i=1,dm
+          call multifab_fill_boundary(mnew(n,i))
+       end do
     end do
-
-    ! compute del dot (-rho*v)
-    call mk_advective_s_fluxdiv(mla,umac,s_face,s_update,dx)
-
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    ! Step 4 - Crank-Nicolson Velocity Corrector
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     ! End Time-Advancement
@@ -219,6 +323,8 @@ contains
           call destroy(s_face(n,i))
           call destroy(gmres_rhs_v(n,i))
           call destroy(chi_face(n,i))
+          call destroy(m_a_fluxdiv(n,i))
+          call destroy(m_d_fluxdiv(n,i))
        end do
     end do
 
