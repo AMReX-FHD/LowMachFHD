943db8e45ef146aa519962ba70e2e86c907d0b34
ajnonaka <AJNonaka@lbl.gov>
2013-06-18 19:56:40 -0700

    some bc work so it does not key off prob_type anymore

--- exec/lowMach_implicit/inhomogeneous_bc_val.f90
+++ exec/lowMach_implicit/inhomogeneous_bc_val.f90
@@ -1,7 +1,7 @@
 module inhomogeneous_bc_val_module
 
   use bl_types
-  use probin_common_module,  only: wallspeed_hi, prob_lo, prob_hi, prob_type
+  use probin_common_module,  only: wallspeed_hi, prob_lo, prob_hi
   use probin_lowmach_module, only: rhobar, c_bc
 
   implicit none
@@ -18,46 +18,53 @@ contains
    real(kind=dp_t), intent(in   ) :: x,y
    real(kind=dp_t)                :: val
 
-   select case (prob_type)
-   case (0)
+   if (comp .eq. 1) then
+      ! x-vel
 
-      if (comp .eq. 4) then
-         ! density
-         if (x .eq. prob_lo(1)) then
-            val = 1.d0/(c_bc(1,1)/rhobar(1) + (1.d0-c_bc(1,1))/rhobar(2))
-         else if (x .eq. prob_hi(1)) then
-            val = 1.d0/(c_bc(1,2)/rhobar(1) + (1.d0-c_bc(1,2))/rhobar(2))
-         else if (y .eq. prob_lo(2)) then
-            val = 1.d0/(c_bc(2,1)/rhobar(1) + (1.d0-c_bc(2,1))/rhobar(2))
-         else if (y .eq. prob_hi(2)) then
-            val = 1.d0/(c_bc(2,2)/rhobar(1) + (1.d0-c_bc(2,2))/rhobar(2))
-         end if
-      else if (comp .eq. 5) then
-         ! concentration
-         if (x .eq. prob_lo(1)) then
-            val = c_bc(1,1)
-         else if (x .eq. prob_hi(1)) then
-            val = c_bc(1,2)
-         else if (y .eq. prob_lo(2)) then
-            val = c_bc(2,1)
-         else if (y .eq. prob_hi(2)) then
-            val = c_bc(2,2)
-         end if
-      else
-         val = 0.d0
-      end if
-
-   case (1)
-
-      if (y .eq. prob_hi(2) .and. comp .eq. 1) then
+      if (y .eq. prob_hi(2)) then
          val = wallspeed_hi(1,2)
       else
          val = 0.d0
       end if
+   
+   else if (comp .eq. 2) then
+      ! y-vel
 
-   case default
       val = 0.d0
-   end select
+
+   else if (comp .eq. 4) then
+      ! density
+
+      if (x .eq. prob_lo(1)) then
+         val = 1.d0/(c_bc(1,1)/rhobar(1) + (1.d0-c_bc(1,1))/rhobar(2))
+      else if (x .eq. prob_hi(1)) then
+         val = 1.d0/(c_bc(1,2)/rhobar(1) + (1.d0-c_bc(1,2))/rhobar(2))
+      else if (y .eq. prob_lo(2)) then
+         val = 1.d0/(c_bc(2,1)/rhobar(1) + (1.d0-c_bc(2,1))/rhobar(2))
+      else if (y .eq. prob_hi(2)) then
+         val = 1.d0/(c_bc(2,2)/rhobar(1) + (1.d0-c_bc(2,2))/rhobar(2))
+      else
+         val = 0.d0
+      end if
+      
+   else if (comp .eq. 5) then
+      ! concentration
+
+      if (x .eq. prob_lo(1)) then
+         val = c_bc(1,1)
+      else if (x .eq. prob_hi(1)) then
+         val = c_bc(1,2)
+      else if (y .eq. prob_lo(2)) then
+         val = c_bc(2,1)
+      else if (y .eq. prob_hi(2)) then
+         val = c_bc(2,2)
+      else
+         val = 0.d0
+      end if
+
+   else
+      val = 0.d0
+   end if
 
  end function inhomogeneous_bc_val_2d
 
@@ -67,60 +74,70 @@ contains
    real(kind=dp_t), intent(in   ) :: x,y,z
    real(kind=dp_t)                :: val
 
-   select case (prob_type)
-   case (0)
-
-      if (comp .eq. 5) then
-         ! density
-         if (x .eq. prob_lo(1)) then
-            val = 1.d0/(c_bc(1,1)/rhobar(1) + (1.d0-c_bc(1,1))/rhobar(2))
-         else if (x .eq. prob_hi(1)) then
-            val = 1.d0/(c_bc(1,2)/rhobar(1) + (1.d0-c_bc(1,2))/rhobar(2))
-         else if (y .eq. prob_lo(2)) then
-            val = 1.d0/(c_bc(2,1)/rhobar(1) + (1.d0-c_bc(2,1))/rhobar(2))
-         else if (y .eq. prob_hi(2)) then
-            val = 1.d0/(c_bc(2,2)/rhobar(1) + (1.d0-c_bc(2,2))/rhobar(2))
-         else if (z .eq. prob_lo(3)) then
-            val = 1.d0/(c_bc(3,1)/rhobar(1) + (1.d0-c_bc(3,1))/rhobar(2))
-         else if (z .eq. prob_hi(3)) then
-            val = 1.d0/(c_bc(3,2)/rhobar(1) + (1.d0-c_bc(3,2))/rhobar(2))
-         end if
-      else if (comp .eq. 6) then
-         ! concentration
-         if (x .eq. prob_lo(1)) then
-            val = c_bc(1,1)
-         else if (x .eq. prob_hi(1)) then
-            val = c_bc(1,2)
-         else if (y .eq. prob_lo(2)) then
-            val = c_bc(2,1)
-         else if (y .eq. prob_hi(2)) then
-            val = c_bc(2,2)
-         else if (z .eq. prob_lo(3)) then
-            val = c_bc(3,1)
-         else if (z .eq. prob_hi(3)) then
-            val = c_bc(3,2)
-         end if
-      else
-         val = 0.d0
-      end if
-
-   case (1)
+   if (comp .eq. 1) then
+      ! x-vel
 
       if (z .eq. prob_hi(3)) then
-         if (comp .eq. 1) then
-            val = wallspeed_hi(1,3)
-         else if (comp .eq. 2) then
-            val = wallspeed_hi(2,3)
-         else
-            val = 0.d0
-         end if
+         val = wallspeed_hi(1,3)
       else
          val = 0.d0
       end if
 
-   case default
+   else if (comp .eq. 2) then
+      ! y-vel
+
+      if (z .eq. prob_hi(3)) then
+         val = wallspeed_hi(2,3)
+      else
+         val = 0.d0
+      end if
+
+   else if (comp .eq. 3) then
+      ! z-vel
+
       val = 0.d0
-   end select
+
+   else if (comp .eq. 5) then
+      ! density
+
+      if (x .eq. prob_lo(1)) then
+         val = 1.d0/(c_bc(1,1)/rhobar(1) + (1.d0-c_bc(1,1))/rhobar(2))
+      else if (x .eq. prob_hi(1)) then
+         val = 1.d0/(c_bc(1,2)/rhobar(1) + (1.d0-c_bc(1,2))/rhobar(2))
+      else if (y .eq. prob_lo(2)) then
+         val = 1.d0/(c_bc(2,1)/rhobar(1) + (1.d0-c_bc(2,1))/rhobar(2))
+      else if (y .eq. prob_hi(2)) then
+         val = 1.d0/(c_bc(2,2)/rhobar(1) + (1.d0-c_bc(2,2))/rhobar(2))
+      else if (z .eq. prob_lo(3)) then
+         val = 1.d0/(c_bc(3,1)/rhobar(1) + (1.d0-c_bc(3,1))/rhobar(2))
+      else if (z .eq. prob_hi(3)) then
+         val = 1.d0/(c_bc(3,2)/rhobar(1) + (1.d0-c_bc(3,2))/rhobar(2))
+      else
+         val = 0.d0
+      end if
+
+   else if (comp .eq. 6) then
+      ! concentration
+
+      if (x .eq. prob_lo(1)) then
+         val = c_bc(1,1)
+      else if (x .eq. prob_hi(1)) then
+         val = c_bc(1,2)
+      else if (y .eq. prob_lo(2)) then
+         val = c_bc(2,1)
+      else if (y .eq. prob_hi(2)) then
+         val = c_bc(2,2)
+      else if (z .eq. prob_lo(3)) then
+         val = c_bc(3,1)
+      else if (z .eq. prob_hi(3)) then
+         val = c_bc(3,2)
+      else
+         val = 0.d0
+      end if
+
+   else
+      val = 0.d0
+   end if
 
  end function inhomogeneous_bc_val_3d
 
