678e9552b4bfed86979164375d18cff5b02004d2
ajnonaka <AJNonaka@lbl.gov>
2013-06-21 16:19:03 -0700

    mk_diffusive_rhoc_fluxdiv now hooks into compute_grad.  The bc special stencil near walls and inlet reservoirs for scalars is handled within compute_grad, so I don't have to do special stencils in mk_diffusive_rhoc_fluxdiv anymore, thus eliminating redudant copies of bc stencils.
    Also, to update the normal velocity bc multifab for reservoirs, I now directly test to see if there is a reservoir conditions, rather than trying to be cute and looking at the combination of conditions on velocity and scalars

--- src_lowMach/mk_diffusive_fluxdiv.f90
+++ src_lowMach/mk_diffusive_fluxdiv.f90
@@ -3,7 +3,7 @@ module mk_diffusive_fluxdiv_module
   use ml_layout_module
   use define_bc_module
   use bc_module
-  use convert_stag_module
+  use div_and_grad_module
   use probin_lowmach_module, only: visc_coef, diff_coef, rhobar
 
   implicit none
@@ -28,11 +28,13 @@ contains
     type(multifab) , intent(inout) :: vel_bc_n(:,:) ! inhomogeneous normal vel value
 
     ! local variables
-    integer :: nlevs,dm,i,n,ng_u,ng_p,ng_s,ng_c,ng_b
+    integer :: nlevs,dm,i,n,ng_u,ng_g,ng_s,ng_c,ng_b
     integer :: lo(mla%dim),hi(mla%dim)
 
     real(kind=dp_t), pointer :: up(:,:,:,:)
-    real(kind=dp_t), pointer :: pp(:,:,:,:)
+    real(kind=dp_t), pointer :: gcx(:,:,:,:)
+    real(kind=dp_t), pointer :: gcy(:,:,:,:)
+    real(kind=dp_t), pointer :: gcz(:,:,:,:)
     real(kind=dp_t), pointer :: spx(:,:,:,:)
     real(kind=dp_t), pointer :: spy(:,:,:,:)
     real(kind=dp_t), pointer :: spz(:,:,:,:)
@@ -43,20 +45,31 @@ contains
     real(kind=dp_t), pointer :: vpy(:,:,:,:)
     real(kind=dp_t), pointer :: vpz(:,:,:,:)
 
-    ng_u = s_update(1)%ng
-    ng_p = prim(1)%ng
-    ng_s = rho_fc(1,1)%ng
-    ng_c = chi_fc(1,1)%ng
-    ng_b = vel_bc_n(1,1)%ng
+    type(multifab) :: gradc(mla%nlevel,mla%dim)
 
     nlevs = mla%nlevel
     dm    = mla%dim
+
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_build_edge(gradc(n,i),mla%la(n),1,0,i)
+       end do
+    end do
+
+    ng_u = s_update(1)%ng
+    ng_s = rho_fc(1,1)%ng
+    ng_c = chi_fc(1,1)%ng
+    ng_b = vel_bc_n(1,1)%ng
+    ng_g = gradc(1,1)%ng
+
+    call compute_grad(mla,prim,gradc,dx,2,scal_bc_comp+1,1,1,the_bc_level)
 
     ! compute del dot (rhoD grad c) and add it to s_update
     do n=1,nlevs
        do i=1,nfabs(prim(n))
           up  => dataptr(s_update(n), i)
-          pp  => dataptr(prim(n), i)
+          gcx => dataptr(gradc(n,1), i)
+          gcy => dataptr(gradc(n,2), i)
           spx => dataptr(rho_fc(n,1), i)
           spy => dataptr(rho_fc(n,2), i)
           cpx => dataptr(chi_fc(n,1), i)
@@ -67,35 +80,45 @@ contains
           hi = upb(get_box(prim(n), i))
           select case (dm)
           case (2)
-             call mk_diffusive_rhoc_fluxdiv_2d(up(:,:,1,out_comp), ng_u, pp(:,:,1,2), ng_p, &
+             call mk_diffusive_rhoc_fluxdiv_2d(up(:,:,1,out_comp), ng_u, &
+                                               gcx(:,:,1,1), gcy(:,:,1,1), ng_g, &
                                                spx(:,:,1,1), spy(:,:,1,1), ng_s, &
                                                cpx(:,:,1,1), cpy(:,:,1,1), ng_c, &
                                                vpx(:,:,1,1), vpy(:,:,1,1), ng_b, &
                                                lo, hi, dx(n,:), &
-                                               the_bc_level(n)%adv_bc_level_array(i,:,:,:))
+                                               the_bc_level(n)%phys_bc_level_array(i,:,:))
           case (3)
+             gcz => dataptr(gradc(n,3), i)
              spz => dataptr(rho_fc(n,3), i)
              cpz => dataptr(chi_fc(n,3), i)
              vpz => dataptr(vel_bc_n(n,3), i)
-             call mk_diffusive_rhoc_fluxdiv_3d(up(:,:,:,out_comp), ng_u, pp(:,:,:,2), ng_p, &
+             call mk_diffusive_rhoc_fluxdiv_3d(up(:,:,:,out_comp), ng_u, &
+                                               gcx(:,:,:,1), gcy(:,:,:,1), gcz(:,:,:,1), ng_g, &
                                                spx(:,:,:,1), spy(:,:,:,1), spz(:,:,:,1), ng_s, &
                                                cpx(:,:,:,1), cpy(:,:,:,1), cpz(:,:,:,1), ng_c, &
                                                vpx(:,:,:,1), vpy(:,:,:,1), vpz(:,:,:,1), ng_b, &
                                                lo, hi, dx(n,:), &
-                                               the_bc_level(n)%adv_bc_level_array(i,:,:,:))
+                                               the_bc_level(n)%phys_bc_level_array(i,:,:))
           end select
+       end do
+    end do
+
+    do n=1,nlevs
+       do i=1,dm
+          call multifab_destroy(gradc(n,i))
        end do
     end do
 
   contains
 
-    subroutine mk_diffusive_rhoc_fluxdiv_2d(s_update,ng_u,c,ng_p,rhox,rhoy,ng_s, &
+    subroutine mk_diffusive_rhoc_fluxdiv_2d(s_update,ng_u,gradcx,gradcy,ng_g,rhox,rhoy,ng_s, &
                                             chix,chiy,ng_c,vel_bc_nx,vel_bc_ny,ng_b, &
-                                            lo,hi,dx,adv_bc)
+                                            lo,hi,dx,bc)
 
-      integer        , intent(in   ) :: lo(:), hi(:), ng_u, ng_p, ng_s, ng_c, ng_b
+      integer        , intent(in   ) :: lo(:), hi(:), ng_u, ng_g, ng_s, ng_c, ng_b
       real(kind=dp_t), intent(inout) ::  s_update(lo(1)-ng_u:,lo(2)-ng_u:)
-      real(kind=dp_t), intent(in   ) ::         c(lo(1)-ng_p:,lo(2)-ng_p:)
+      real(kind=dp_t), intent(in   ) ::    gradcx(lo(1)-ng_g:,lo(2)-ng_g:)
+      real(kind=dp_t), intent(in   ) ::    gradcy(lo(1)-ng_g:,lo(2)-ng_g:)
       real(kind=dp_t), intent(in   ) ::      rhox(lo(1)-ng_s:,lo(2)-ng_s:)
       real(kind=dp_t), intent(in   ) ::      rhoy(lo(1)-ng_s:,lo(2)-ng_s:)
       real(kind=dp_t), intent(in   ) ::      chix(lo(1)-ng_c:,lo(2)-ng_c:)
@@ -103,7 +126,7 @@ contains
       real(kind=dp_t), intent(inout) :: vel_bc_nx(lo(1)-ng_b:,lo(2)-ng_b:)
       real(kind=dp_t), intent(inout) :: vel_bc_ny(lo(1)-ng_b:,lo(2)-ng_b:)
       real(kind=dp_t), intent(in   ) :: dx(:)
-      integer        , intent(in   ) :: adv_bc(:,:,:)
+      integer        , intent(in   ) :: bc(:,:)
 
       ! local
       integer :: i,j
@@ -119,30 +142,16 @@ contains
       do j=lo(2),hi(2)
          do i=lo(1),hi(1)+1
             ! fluxx = rhoD * grad c
-            fluxx(i,j) = rhox(i,j)*chix(i,j) * (c(i,j)-c(i-1,j)) / dx(1)
+            fluxx(i,j) = rhox(i,j)*chix(i,j) * gradcx(i,j)
          end do
       end do
 
-      ! alter grad(c) stencil at boundary since ghost value represents value at boundary
-      if (adv_bc(1,1,4) .eq. FOEXTRAP .or. adv_bc(1,1,4) .eq. EXT_DIR) then
-         i=lo(1)
-         do j=lo(2),hi(2)
-            fluxx(i,j) = rhox(i,j)*chix(i,j) * (c(i,j)-c(i-1,j)) / (0.5d0*dx(1))
-         end do
-      end if
-      if (adv_bc(1,2,4) .eq. FOEXTRAP .or. adv_bc(1,2,4) .eq. EXT_DIR) then
-         i=hi(1)+1
-         do j=lo(2),hi(2)
-            fluxx(i,j) = rhox(i,j)*chix(i,j) * (c(i,j)-c(i-1,j)) / (0.5d0*dx(1))
-         end do
-      end if
-
       ! update umac bc based on diffusive flux at boundary
-      if (adv_bc(1,1,1) .eq. DIR_VEL .and. adv_bc(1,1,4) .eq. EXT_DIR) then
+      if (bc(1,1) .eq. NO_SLIP_RESERVOIR .or. bc(1,1) .eq. SLIP_RESERVOIR) then
          vel_bc_nx(lo(1),lo(2):hi(2)) = vel_bc_nx(lo(1),lo(2):hi(2)) &
               + S_fac*fluxx(lo(1),lo(2):hi(2))
       end if
-      if (adv_bc(1,2,1) .eq. DIR_VEL .and. adv_bc(1,2,4) .eq. EXT_DIR) then
+      if (bc(1,2) .eq. NO_SLIP_RESERVOIR .or. bc(1,2) .eq. SLIP_RESERVOIR) then
          vel_bc_nx(hi(1)+1,lo(2):hi(2)) = vel_bc_nx(hi(1)+1,lo(2):hi(2)) &
               + S_fac*fluxx(hi(1)+1,lo(2):hi(2))
       end if
@@ -151,30 +160,16 @@ contains
       do j=lo(2),hi(2)+1
          do i=lo(1),hi(1)
             ! fluxy = rhoD * grad c
-            fluxy(i,j) = rhoy(i,j)*chiy(i,j) * (c(i,j)-c(i,j-1)) / dx(2)
+            fluxy(i,j) = rhoy(i,j)*chiy(i,j) * gradcy(i,j)
          end do
       end do
 
-      ! alter grad(c) stencil at boundary since ghost value represents value at boundary
-      if (adv_bc(2,1,4) .eq. FOEXTRAP .or. adv_bc(2,1,4) .eq. EXT_DIR) then
-         j=lo(2)
-         do i=lo(1),hi(1)
-            fluxy(i,j) = rhoy(i,j)*chiy(i,j) * (c(i,j)-c(i,j-1)) / (0.5d0*dx(2))
-         end do
-      end if
-      if (adv_bc(2,2,4) .eq. FOEXTRAP .or. adv_bc(2,2,4) .eq. EXT_DIR) then
-         j=hi(2)+1
-         do i=lo(1),hi(1)
-            fluxy(i,j) = rhoy(i,j)*chiy(i,j) * (c(i,j)-c(i,j-1)) / (0.5d0*dx(2))
-         end do
-      end if
-
       ! update vmac bc based on diffusive flux at boundary
-      if (adv_bc(2,1,2) .eq. DIR_VEL .and. adv_bc(2,1,4) .eq. EXT_DIR) then
+      if (bc(2,1) .eq. NO_SLIP_RESERVOIR .or. bc(2,1) .eq. SLIP_RESERVOIR) then
          vel_bc_ny(lo(1):hi(1),lo(2)) = vel_bc_ny(lo(1):hi(1),lo(2)) + &
               S_fac*fluxy(lo(1):hi(1),lo(2))
       end if
-      if (adv_bc(2,2,2) .eq. DIR_VEL .and. adv_bc(2,2,4) .eq. EXT_DIR) then
+      if (bc(2,2) .eq. NO_SLIP_RESERVOIR .or. bc(2,2) .eq. SLIP_RESERVOIR) then
          vel_bc_ny(lo(1):hi(1),hi(2)+1) = vel_bc_ny(lo(1):hi(1),hi(2)+1) + &
               S_fac*fluxy(lo(1):hi(1),hi(2)+1)
       end if
@@ -190,13 +185,16 @@ contains
 
     end subroutine mk_diffusive_rhoc_fluxdiv_2d
 
-    subroutine mk_diffusive_rhoc_fluxdiv_3d(s_update,ng_u,c,ng_p,rhox,rhoy,rhoz,ng_s, &
+    subroutine mk_diffusive_rhoc_fluxdiv_3d(s_update,ng_u,gradcx,gradcy,gradcz,ng_g, &
+                                            rhox,rhoy,rhoz,ng_s, &
                                             chix,chiy,chiz,ng_c,vel_bc_nx, &
-                                            vel_bc_ny,vel_bc_nz,ng_b,lo,hi,dx,adv_bc)
+                                            vel_bc_ny,vel_bc_nz,ng_b,lo,hi,dx,bc)
 
-      integer        , intent(in   ) :: lo(:), hi(:), ng_u, ng_p, ng_s, ng_c,ng_b
+      integer        , intent(in   ) :: lo(:), hi(:), ng_u, ng_g, ng_s, ng_c, ng_b
       real(kind=dp_t), intent(inout) ::  s_update(lo(1)-ng_u:,lo(2)-ng_u:,lo(3)-ng_u:)
-      real(kind=dp_t), intent(in   ) ::         c(lo(1)-ng_p:,lo(2)-ng_p:,lo(3)-ng_p:)
+      real(kind=dp_t), intent(in   ) ::    gradcx(lo(1)-ng_g:,lo(2)-ng_g:,lo(3)-ng_g:)
+      real(kind=dp_t), intent(in   ) ::    gradcy(lo(1)-ng_g:,lo(2)-ng_g:,lo(3)-ng_g:)
+      real(kind=dp_t), intent(in   ) ::    gradcz(lo(1)-ng_g:,lo(2)-ng_g:,lo(3)-ng_g:)
       real(kind=dp_t), intent(in   ) ::      rhox(lo(1)-ng_s:,lo(2)-ng_s:,lo(3)-ng_s:)
       real(kind=dp_t), intent(in   ) ::      rhoy(lo(1)-ng_s:,lo(2)-ng_s:,lo(3)-ng_s:)
       real(kind=dp_t), intent(in   ) ::      rhoz(lo(1)-ng_s:,lo(2)-ng_s:,lo(3)-ng_s:)
@@ -207,7 +205,7 @@ contains
       real(kind=dp_t), intent(inout) :: vel_bc_ny(lo(1)-ng_b:,lo(2)-ng_b:,lo(3)-ng_b:)
       real(kind=dp_t), intent(inout) :: vel_bc_nz(lo(1)-ng_b:,lo(2)-ng_b:,lo(3)-ng_b:)
       real(kind=dp_t), intent(in   ) :: dx(:)
-      integer        , intent(in   ) :: adv_bc(:,:,:)
+      integer        , intent(in   ) :: bc(:,:)
 
       ! local
       integer :: i,j,k
@@ -225,35 +223,17 @@ contains
          do j=lo(2),hi(2)
             do i=lo(1),hi(1)+1
                ! fluxx = rhoD * grad c
-               fluxx(i,j,k) = rhox(i,j,k)*chix(i,j,k) * (c(i,j,k) - c(i-1,j,k)) / dx(1)
+               fluxx(i,j,k) = rhox(i,j,k)*chix(i,j,k) * gradcx(i,j,k)
             end do
          end do
       end do
 
-      ! alter grad(c) stencil at boundary since ghost value represents value at boundary
-      if (adv_bc(1,1,5) .eq. FOEXTRAP .or. adv_bc(1,1,5) .eq. EXT_DIR) then
-         i=lo(1)
-         do k=lo(3),hi(3)
-            do j=lo(2),hi(2)
-               fluxx(i,j,k) = rhox(i,j,k)*chix(i,j,k) * (c(i,j,k)-c(i-1,j,k)) / (0.5d0*dx(1))
-            end do
-         end do
-      end if
-      if (adv_bc(1,2,5) .eq. FOEXTRAP .or. adv_bc(1,2,5) .eq. EXT_DIR) then
-         i=hi(1)+1
-         do k=lo(3),hi(3)
-            do j=lo(2),hi(2)
-               fluxx(i,j,k) = rhox(i,j,k)*chix(i,j,k) * (c(i,j,k)-c(i-1,j,k)) / (0.5d0*dx(1))
-            end do
-         end do
-      end if
-
       ! update umac bc based on diffusive flux at boundary
-      if (adv_bc(1,1,1) .eq. DIR_VEL .and. adv_bc(1,1,5) .eq. EXT_DIR) then
+      if (bc(1,1) .eq. NO_SLIP_RESERVOIR .or. bc(1,1) .eq. SLIP_RESERVOIR) then
          vel_bc_nx(lo(1),lo(2):hi(2),lo(3):hi(3)) = vel_bc_nx(lo(1),lo(2):hi(2),lo(3):hi(3)) &
               + S_fac*fluxx(lo(1),lo(2):hi(2),lo(3):hi(3))
       end if
-      if (adv_bc(1,2,1) .eq. DIR_VEL .and. adv_bc(1,2,5) .eq. EXT_DIR) then
+      if (bc(1,2) .eq. NO_SLIP_RESERVOIR .or. bc(1,2) .eq. SLIP_RESERVOIR) then
          vel_bc_nx(hi(1)+1,lo(2):hi(2),lo(3):hi(3)) = vel_bc_nx(hi(1)+1,lo(2):hi(2),lo(3):hi(3)) &
               + S_fac*fluxx(hi(1)+1,lo(2):hi(2),lo(3):hi(3))
       end if
@@ -263,35 +243,17 @@ contains
          do j=lo(2),hi(2)+1
             do i=lo(1),hi(1)
                ! fluxy = rhoD * grad c
-               fluxy(i,j,k) = rhoy(i,j,k)*chiy(i,j,k) * (c(i,j,k) - c(i,j-1,k)) / dx(2)
+               fluxy(i,j,k) = rhoy(i,j,k)*chiy(i,j,k) * gradcy(i,j,k)
             end do
          end do
       end do
 
-      ! alter grad(c) stencil at boundary since ghost value represents value at boundary
-      if (adv_bc(2,1,5) .eq. FOEXTRAP .or. adv_bc(2,1,5) .eq. EXT_DIR) then
-         j=lo(2)
-         do k=lo(3),hi(3)
-            do i=lo(1),hi(1)
-               fluxy(i,j,k) = rhoy(i,j,k)*chiy(i,j,k) * (c(i,j,k)-c(i,j-1,k)) / (0.5d0*dx(2))
-            end do
-         end do
-      end if
-      if (adv_bc(2,2,5) .eq. FOEXTRAP .or. adv_bc(2,2,5) .eq. EXT_DIR) then
-         j=hi(2)+1
-         do k=lo(3),hi(3)
-            do i=lo(1),hi(1)
-               fluxy(i,j,k) = rhoy(i,j,k)*chiy(i,j,k) * (c(i,j,k)-c(i,j-1,k)) / (0.5d0*dx(2))
-            end do
-         end do
-      end if
-
       ! update vmac bc based on diffusive flux at boundary
-      if (adv_bc(2,1,2) .eq. DIR_VEL .and. adv_bc(2,1,5) .eq. EXT_DIR) then
+      if (bc(2,1) .eq. NO_SLIP_RESERVOIR .or. bc(2,1) .eq. SLIP_RESERVOIR) then
          vel_bc_ny(lo(1):hi(1),lo(2),lo(3):hi(3)) = vel_bc_ny(lo(1):hi(1),lo(2),lo(3):hi(3)) + &
               S_fac*fluxy(lo(1):hi(1),lo(2),lo(3):hi(3))
       end if
-      if (adv_bc(2,2,2) .eq. DIR_VEL .and. adv_bc(2,2,5) .eq. EXT_DIR) then
+      if (bc(2,2) .eq. NO_SLIP_RESERVOIR .or. bc(2,2) .eq. SLIP_RESERVOIR) then
          vel_bc_ny(lo(1):hi(1),hi(2)+1,lo(3):hi(3)) = vel_bc_ny(lo(1):hi(1),hi(2)+1,lo(3):hi(3)) + &
               S_fac*fluxy(lo(1):hi(1),hi(2)+1,lo(3):hi(3))
       end if
@@ -301,35 +263,17 @@ contains
          do j=lo(2),hi(2)
             do i=lo(1),hi(1)
                ! fluxz = rhoD * grad c
-               fluxz(i,j,k) = rhoz(i,j,k)*chiz(i,j,k) * (c(i,j,k) - c(i,j,k-1)) / dx(3)
+               fluxz(i,j,k) = rhoz(i,j,k)*chiz(i,j,k) * gradcz(i,j,k)
             end do
          end do
       end do
 
-      ! alter grad(c) stencil at boundary since ghost value represents value at boundary
-      if (adv_bc(3,1,5) .eq. FOEXTRAP .or. adv_bc(3,1,5) .eq. EXT_DIR) then
-         k=lo(3)
-         do j=lo(2),hi(2)
-            do i=lo(1),hi(1)
-               fluxz(i,j,k) = rhoz(i,j,k)*chiz(i,j,k) * (c(i,j,k)-c(i,j,k-1)) / (0.5d0*dx(3))
-            end do
-         end do
-      end if
-      if (adv_bc(3,2,5) .eq. FOEXTRAP .or. adv_bc(3,2,5) .eq. EXT_DIR) then
-         k=hi(3)+1
-         do j=lo(2),hi(2)
-            do i=lo(1),hi(1)
-               fluxz(i,j,k) = rhoz(i,j,k)*chiz(i,j,k) * (c(i,j,k)-c(i,j,k-1)) / (0.5d0*dx(3))
-            end do
-         end do
-      end if
-
       ! update wmac bc based on diffusive flux at boundary
-      if (adv_bc(3,1,3) .eq. DIR_VEL .and. adv_bc(3,1,5) .eq. EXT_DIR) then
+      if (bc(3,1) .eq. NO_SLIP_RESERVOIR .or. bc(3,1) .eq. SLIP_RESERVOIR) then
          vel_bc_nz(lo(1):hi(1),lo(2):hi(2),lo(3)) = vel_bc_nz(lo(1):hi(1),lo(2):hi(2),lo(3)) + &
               S_fac*fluxz(lo(1):hi(1),lo(2):hi(2),lo(3))
       end if
-      if (adv_bc(3,2,3) .eq. DIR_VEL .and. adv_bc(3,2,5) .eq. EXT_DIR) then
+      if (bc(3,2) .eq. NO_SLIP_RESERVOIR .or. bc(3,2) .eq. SLIP_RESERVOIR) then
          vel_bc_nz(lo(1):hi(1),lo(2):hi(2),hi(3)+1) = vel_bc_nz(lo(1):hi(1),lo(2):hi(2),hi(3)+1) + &
               S_fac*fluxz(lo(1):hi(1),lo(2):hi(2),hi(3)+1)
       end if
