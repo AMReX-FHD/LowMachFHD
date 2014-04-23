19f9c9751891779c80527c9fd032c2b2a8fca43d
ajnonaka <AJNonaka@lbl.gov>
2014-04-23 13:02:03 -0700

    more checkpoint progress

--- src_binaryLM/checkpoint.f90
+++ src_binaryLM/checkpoint.f90
@@ -7,12 +7,13 @@ module checkpoint_module
   use bl_IO_module
   use fab_module
   use fabio_module, only: fabio_mkdir, fabio_ml_multifab_write_d
+  use probin_common_module, only: dim_in
 
   implicit none
 
   private
 
-  public :: write_checkfile, checkpoint_read
+  public :: write_checkfile, checkpoint_read_2d, checkpoint_read_3d
 
 contains
 
@@ -120,97 +121,99 @@ contains
     deallocate(chkdata)
     deallocate(chkdata_edge)
 
+  contains
+
+    subroutine checkpoint_write(nlevs_in, dirname, mfs, mfs_edge, mfs_vel_bc_t, rrs, time_in, dt_in)
+      
+      integer         , intent(in) :: nlevs_in
+      type(multifab)  , intent(in) :: mfs(:)
+      type(multifab)  , intent(in) :: mfs_edge(:,:)
+      type(multifab)  , intent(in) :: mfs_vel_bc_t(:,:)
+      integer         , intent(in) :: rrs(:,:)
+      character(len=*), intent(in) :: dirname
+      real(kind=dp_t) , intent(in) :: time_in, dt_in
+
+      integer :: n
+      character(len=128) :: header, sd_name
+      integer :: un, dm
+
+      integer         :: nlevs
+      real(kind=dp_t) :: time, dt
+
+      namelist /chkpoint/ time
+      namelist /chkpoint/ dt
+      namelist /chkpoint/ nlevs
+
+      dm = dim_in
+      if ( parallel_IOProcessor() ) call fabio_mkdir(dirname)
+
+      call parallel_barrier() ! All CPUs have to wait till the directory is built.
+
+      write(unit=sd_name, fmt='(a,"/State")') trim(dirname)
+      call fabio_ml_multifab_write_d(mfs, rrs(:,1), sd_name)
+
+      if (parallel_IOProcessor()) then
+         print *,'Writing cc state to checkpoint file ',trim(sd_name)
+         print *,' '
+      end if
+
+      write(unit=sd_name, fmt='(a,"/State_edgex")') trim(dirname)
+      call fabio_ml_multifab_write_d(mfs_edge(:,1), rrs(:,1), sd_name)
+
+      write(unit=sd_name, fmt='(a,"/State_edgey")') trim(dirname)
+      call fabio_ml_multifab_write_d(mfs_edge(:,2), rrs(:,1), sd_name)
+
+      if (dm .eq. 3) then
+         write(unit=sd_name, fmt='(a,"/State_edgez")') trim(dirname)
+         call fabio_ml_multifab_write_d(mfs_edge(:,3), rrs(:,1), sd_name)
+      end if
+
+      write(unit=sd_name, fmt='(a,"/State_vel_bc_t1")') trim(dirname)
+      call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,1), rrs(:,1), sd_name)
+
+      write(unit=sd_name, fmt='(a,"/State_vel_bc_t2")') trim(dirname)
+      call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,2), rrs(:,1), sd_name)
+
+      if (dm .eq. 3) then
+
+         write(unit=sd_name, fmt='(a,"/State_vel_bc_t3")') trim(dirname)
+         call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,3), rrs(:,1), sd_name)
+
+         write(unit=sd_name, fmt='(a,"/State_vel_bc_t4")') trim(dirname)
+         call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,4), rrs(:,1), sd_name)
+
+         write(unit=sd_name, fmt='(a,"/State_vel_bc_t5")') trim(dirname)
+         call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,5), rrs(:,1), sd_name)
+
+         write(unit=sd_name, fmt='(a,"/State_vel_bc_t6")') trim(dirname)
+         call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,6), rrs(:,1), sd_name)
+
+      end if
+
+      time  = time_in
+      dt = dt_in
+      nlevs = nlevs_in
+
+      if (parallel_IOProcessor()) then
+         header = "Header"
+         un = unit_new()
+         open(unit=un, &
+              file = trim(dirname) // "/" // trim(header), &
+              form = "formatted", access = "sequential", &
+              status = "replace", action = "write")
+         write(unit=un, nml = chkpoint)
+         do n = 1,nlevs-1
+            write(unit=un,fmt=*) rrs(n,1)
+         end do
+         close(un)
+      end if
+
+    end subroutine checkpoint_write
+
   end subroutine write_checkfile
 
-  subroutine checkpoint_write(nlevs_in, dirname, mfs, mfs_edge, mfs_vel_bc_t, rrs, time_in, dt_in)
-
-    integer         , intent(in) :: nlevs_in
-    type(multifab)  , intent(in) :: mfs(:)
-    type(multifab)  , intent(in) :: mfs_edge(:,:)
-    type(multifab)  , intent(in) :: mfs_vel_bc_t(:,:)
-    integer         , intent(in) :: rrs(:,:)
-    character(len=*), intent(in) :: dirname
-    real(kind=dp_t) , intent(in) :: time_in, dt_in
-
-    integer :: n
-    character(len=128) :: header, sd_name
-    integer :: un, dm
-
-    integer         :: nlevs
-    real(kind=dp_t) :: time, dt
-
-    namelist /chkpoint/ time
-    namelist /chkpoint/ dt
-    namelist /chkpoint/ nlevs
-
-    dm = get_dim(mfs(1))
-    if ( parallel_IOProcessor() ) call fabio_mkdir(dirname)
-
-    call parallel_barrier() ! All CPUs have to wait till the directory is built.
-
-    write(unit=sd_name, fmt='(a,"/State")') trim(dirname)
-    call fabio_ml_multifab_write_d(mfs, rrs(:,1), sd_name)
-
-    if (parallel_IOProcessor()) then
-      print *,'Writing cc state to checkpoint file ',trim(sd_name)
-      print *,' '
-    end if
-
-    write(unit=sd_name, fmt='(a,"/State_edgex")') trim(dirname)
-    call fabio_ml_multifab_write_d(mfs_edge(:,1), rrs(:,1), sd_name)
-
-    write(unit=sd_name, fmt='(a,"/State_edgey")') trim(dirname)
-    call fabio_ml_multifab_write_d(mfs_edge(:,2), rrs(:,1), sd_name)
-
-    if (dm .eq. 3) then
-       write(unit=sd_name, fmt='(a,"/State_edgez")') trim(dirname)
-       call fabio_ml_multifab_write_d(mfs_edge(:,3), rrs(:,1), sd_name)
-    end if
-
-    write(unit=sd_name, fmt='(a,"/State_vel_bc_t1")') trim(dirname)
-    call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,1), rrs(:,1), sd_name)
-
-    write(unit=sd_name, fmt='(a,"/State_vel_bc_t2")') trim(dirname)
-    call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,2), rrs(:,1), sd_name)
-
-    if (dm .eq. 3) then
-
-       write(unit=sd_name, fmt='(a,"/State_vel_bc_t3")') trim(dirname)
-       call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,3), rrs(:,1), sd_name)
-
-       write(unit=sd_name, fmt='(a,"/State_vel_bc_t4")') trim(dirname)
-       call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,4), rrs(:,1), sd_name)
-
-       write(unit=sd_name, fmt='(a,"/State_vel_bc_t5")') trim(dirname)
-       call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,5), rrs(:,1), sd_name)
-
-       write(unit=sd_name, fmt='(a,"/State_vel_bc_t6")') trim(dirname)
-       call fabio_ml_multifab_write_d(mfs_vel_bc_t(:,6), rrs(:,1), sd_name)
-
-    end if
-
-    time  = time_in
-    dt = dt_in
-    nlevs = nlevs_in
-
-    if (parallel_IOProcessor()) then
-       header = "Header"
-       un = unit_new()
-       open(unit=un, &
-            file = trim(dirname) // "/" // trim(header), &
-            form = "formatted", access = "sequential", &
-            status = "replace", action = "write")
-       write(unit=un, nml = chkpoint)
-       do n = 1,nlevs-1
-          write(unit=un,fmt=*) rrs(n,1)
-       end do
-       close(un)
-    end if
-
-  end subroutine checkpoint_write
-
-  subroutine checkpoint_read(mfs, mfs_edgex, mfs_edgey, mfs_edgez, dirname, &
-                             rrs_out, time_out, dt_out, nlevs_out)
+  subroutine checkpoint_read_2d(mfs, mfs_edgex, mfs_edgey, mfs_vel_bc_t1, mfs_vel_bc_t2, &
+                                dirname, rrs_out, time_out, dt_out, nlevs_out)
 
     use bl_IO_module
     use fab_module
@@ -220,7 +223,8 @@ contains
     type(multifab)  ,                pointer :: mfs(:)
     type(multifab)  ,                pointer :: mfs_edgex(:)
     type(multifab)  ,                pointer :: mfs_edgey(:)
-    type(multifab)  ,                pointer :: mfs_edgez(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t1(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t2(:)
     character(len=*), intent(in   )          :: dirname
     integer         , intent(  out)          :: nlevs_out
     real(kind=dp_t) , intent(  out)          :: time_out, dt_out
@@ -228,7 +232,7 @@ contains
 
     character(len=128) :: header, sd_name
 
-    integer :: n, un, dm, nlevs
+    integer :: n, un, nlevs
     integer, pointer :: rrs(:)
 
     real(kind=dp_t) :: time, dt
@@ -259,7 +263,81 @@ contains
     write(unit=sd_name, fmt='(a,"/State")') trim(dirname)
     call fabio_ml_multifab_read_d(mfs, sd_name)
 
-    dm = get_dim(mfs(1))
+    write(unit=sd_name, fmt='(a,"/State_edgex")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_edgex, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_edgey")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_edgey, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t1")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t1, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t2")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t2, sd_name)
+
+    rrs_out(1:nlevs-1) = rrs(1:nlevs-1)
+
+    deallocate(rrs)
+
+  end subroutine checkpoint_read_2d
+
+  subroutine checkpoint_read_3d(mfs, mfs_edgex, mfs_edgey, mfs_edgez, &
+                                mfs_vel_bc_t1, mfs_vel_bc_t2, mfs_vel_bc_t3, &
+                                mfs_vel_bc_t4, mfs_vel_bc_t5, mfs_vel_bc_t6, &
+                                dirname, rrs_out, time_out, dt_out, nlevs_out)
+
+    use bl_IO_module
+    use fab_module
+    use fabio_module, only: fabio_ml_multifab_read_d
+    use parallel
+
+    type(multifab)  ,                pointer :: mfs(:)
+    type(multifab)  ,                pointer :: mfs_edgex(:)
+    type(multifab)  ,                pointer :: mfs_edgey(:)
+    type(multifab)  ,                pointer :: mfs_edgez(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t1(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t2(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t3(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t4(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t5(:)
+    type(multifab)  ,                pointer :: mfs_vel_bc_t6(:)
+    character(len=*), intent(in   )          :: dirname
+    integer         , intent(  out)          :: nlevs_out
+    real(kind=dp_t) , intent(  out)          :: time_out, dt_out
+    integer         , intent(  out)          :: rrs_out(:)
+
+    character(len=128) :: header, sd_name
+
+    integer :: n, un, nlevs
+    integer, pointer :: rrs(:)
+
+    real(kind=dp_t) :: time, dt
+
+    namelist /chkpoint/ nlevs
+    namelist /chkpoint/ time
+    namelist /chkpoint/ dt
+
+!   First read the header information
+    header = "Header"
+    un = unit_new()
+    open(unit=un, &
+         file = trim(dirname) // "/" // trim(header), &
+         status = "old", &
+         action = "read")
+    read(unit=un, nml = chkpoint)
+    allocate(rrs(nlevs-1))
+    do n = 1,nlevs-1
+       read(unit=un,fmt=*) rrs(n)
+    end do
+    close(un)
+
+     time_out = time
+       dt_out = dt
+    nlevs_out = nlevs
+
+!   Read the state data into a multilevel multifab.
+    write(unit=sd_name, fmt='(a,"/State")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs, sd_name)
 
     write(unit=sd_name, fmt='(a,"/State_edgex")') trim(dirname)
     call fabio_ml_multifab_read_d(mfs_edgex, sd_name)
@@ -267,15 +345,31 @@ contains
     write(unit=sd_name, fmt='(a,"/State_edgey")') trim(dirname)
     call fabio_ml_multifab_read_d(mfs_edgey, sd_name)
 
-    if (dm .eq. 3) then
-       write(unit=sd_name, fmt='(a,"/State_edgez")') trim(dirname)
-       call fabio_ml_multifab_read_d(mfs_edgez, sd_name)
-    end if
+    write(unit=sd_name, fmt='(a,"/State_edgez")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_edgez, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t1")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t1, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t2")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t2, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t3")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t3, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t4")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t4, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t5")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t5, sd_name)
+
+    write(unit=sd_name, fmt='(a,"/State_vel_bc_t6")') trim(dirname)
+    call fabio_ml_multifab_read_d(mfs_vel_bc_t6, sd_name)
 
     rrs_out(1:nlevs-1) = rrs(1:nlevs-1)
 
     deallocate(rrs)
 
-  end subroutine checkpoint_read
+  end subroutine checkpoint_read_3d
 
 end module checkpoint_module
--- src_binaryLM/restart.f90
+++ src_binaryLM/restart.f90
@@ -3,6 +3,8 @@ module restart_module
   use bl_types
   use multifab_module
   use ml_layout_module
+  use checkpoint_module
+  use probin_common_module, only: dim_in
 
   implicit none
 
@@ -12,10 +14,12 @@ module restart_module
 
 contains
 
-  subroutine fill_restart_data(restart_int,mba,chkdata,chkdata_edgex, &
-                               chkdata_edgey,chkdata_edgez,time,dt)
+  subroutine fill_restart_data(restart_int,mba,chkdata, &
+                               chkdata_edgex,chkdata_edgey,chkdata_edgez, &
+                               chkdata_vel_bc_t1,chkdata_vel_bc_t2,chkdata_vel_bc_t3, &
+                               chkdata_vel_bc_t4,chkdata_vel_bc_t5,chkdata_vel_bc_t6, &
+                               time,dt)
 
-    use checkpoint_module
 
     integer          , intent(in   ) :: restart_int
     real(dp_t)       , intent(  out) :: time,dt
@@ -25,18 +29,35 @@ contains
     type(multifab)   , pointer        :: chkdata_edgex(:)
     type(multifab)   , pointer        :: chkdata_edgey(:)
     type(multifab)   , pointer        :: chkdata_edgez(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t1(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t2(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t3(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t4(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t5(:)
+    type(multifab)   , pointer        :: chkdata_vel_bc_t6(:)
     character(len=9)                  :: sd_name
     integer                           :: n,nlevs,dm
     integer                           :: rrs(10)
 
+    dm = dim_in
+
     write(unit=sd_name,fmt='("chk",i6.6)') restart_int
 
-    if ( parallel_IOProcessor() ) &
+    if ( parallel_IOProcessor() ) then
        print *,'Reading ',sd_name,' to get state data for restart'
-    call checkpoint_read(chkdata, chkdata_edgex, chkdata_edgey, chkdata_edgez, &
-                         sd_name, rrs, time, dt, nlevs)
+    end if
 
-    dm = get_dim(chkdata(1))
+    if (dm .eq. 2) then
+       call checkpoint_read_2d(chkdata, chkdata_edgex, chkdata_edgey, &
+                               chkdata_vel_bc_t1, chkdata_vel_bc_t2, &
+                               sd_name, rrs, time, dt, nlevs)
+    else if (dm .eq. 3) then
+       call checkpoint_read_3d(chkdata, chkdata_edgex, chkdata_edgey, chkdata_edgez, &
+                               chkdata_vel_bc_t1, chkdata_vel_bc_t2, chkdata_vel_bc_t3, &
+                               chkdata_vel_bc_t4, chkdata_vel_bc_t5, chkdata_vel_bc_t6, &
+                               sd_name, rrs, time, dt, nlevs)
+
+    end if
 
     call build(mba,nlevs,dm)
     mba%pd(1) =  bbox(get_boxarray(chkdata(1)))
