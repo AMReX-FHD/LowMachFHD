d7eef92477b1d13dcde606962d14baab779c5a0c
ajnonaka <AJNonaka@lbl.gov>
2014-05-19 09:47:57 -0700

    added averaged and shifted umac to plotfile

--- src_multiSpecLM/write_plotfileLM.f90
+++ src_multiSpecLM/write_plotfileLM.f90
@@ -3,6 +3,7 @@ module write_plotfileLM_module
   use ml_layout_module
   use multifab_module
   use fabio_module
+  use convert_stag_module
   use probin_multispecies_module, only: nspecies
   use probin_common_module, only: prob_lo, prob_hi
 
@@ -10,12 +11,13 @@ module write_plotfileLM_module
 
 contains
   
-  subroutine write_plotfileLM(mla,name,rho,rhotot,Temp,istep,dx,time)
+  subroutine write_plotfileLM(mla,name,rho,rhotot,Temp,umac,istep,dx,time)
 
     type(ml_layout),    intent(in)  :: mla
     character(len=*),   intent(in)  :: name
     type(multifab),     intent(in)  :: rho(:)
     type(multifab),     intent(in)  :: rhotot(:)
+    type(multifab),     intent(in)  :: umac(:,:)
     type(multifab),     intent(in)  :: Temp(:)
     integer,            intent(in)  :: istep
     real(kind=dp_t),    intent(in)  :: dx(:,:),time
@@ -23,14 +25,15 @@ contains
     ! local variables
     character(len=20), allocatable  :: plot_names(:)
     character(len=20)               :: plotfile_name
-    integer                         :: n,nlevs
+    integer                         :: i,dm,n,nlevs
 
     ! multifab of size nlevs  
     type(multifab), allocatable     :: plotdata(:)
 
     nlevs = mla%nlevel
+    dm = mla%dim
   
-    allocate(plot_names(nspecies+2)) ! rho + species + Temp
+    allocate(plot_names(nspecies+2*dm+2)) ! rho + species + Temp + dm (averaged umac) + dm (shifted umac)
     allocate(plotdata(nlevs))
  
     plot_names(1) = "rho"
@@ -38,18 +41,34 @@ contains
        write(plot_names(n+1),'(a,i0)') "rho", n
     enddo
     plot_names(nspecies+2) = "Temp"
+    plot_names(nspecies+3) = "averaged_velx"
+    plot_names(nspecies+4) = "averaged_vely"
+    if (dm > 2) plot_names(nspecies+5) = "averaged_velz"
+    plot_names(nspecies+dm+3) = "shifted_velx"
+    plot_names(nspecies+dm+4) = "shifted_velx"
+    if (dm > 2) plot_names(nspecies+dm+5) = "shifted_velx"
 
-    ! build plotdata for nspecies+2 and 0 ghost cells
+    ! build plotdata for nspecies+2*dm+2 and 0 ghost cells
     do n=1,nlevs
-       call multifab_build(plotdata(n),mla%la(n),nspecies+2,0)
+       call multifab_build(plotdata(n),mla%la(n),nspecies+2*dm+2,0)
     enddo
     
-    ! copy the state into plotdata
+    ! copy rhotot, rho, and Temp into plotdata
     do n = 1,nlevs
        call multifab_copy_c(plotdata(n),1         ,rhotot(n),1,       1,0)
        call multifab_copy_c(plotdata(n),2         ,rho(n)   ,1,nspecies,0)
        call multifab_copy_c(plotdata(n),nspecies+2,Temp(n)  ,1,1       ,0)
     enddo
+
+    ! vel averaged
+    do i=1,dm
+       call average_face_to_cc(mla,umac(:,i),1,plotdata,nspecies+2+i,1)
+    end do
+
+    ! vel shifted
+    do i=1,dm
+       call shift_face_to_cc(mla,umac(:,i),1,plotdata,nspecies+dm+2+i,1)
+    end do
     
     ! define the name of the plotfile that will be written
     write(unit=plotfile_name,fmt='(a,i6.6)') name, istep
