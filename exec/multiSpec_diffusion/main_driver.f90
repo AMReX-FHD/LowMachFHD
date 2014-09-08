43f7018e5f738e1ee920d8d7ad2a7355a5af0d19
ajnonaka <AJNonaka@lbl.gov>
2014-09-08 15:07:11 -0700

    get this compiling again

--- exec/multiSpec_diffusion/main_driver.f90
+++ exec/multiSpec_diffusion/main_driver.f90
@@ -19,11 +19,11 @@ subroutine main_driver()
                                   k_B, max_grid_size, n_steps_save_stats, n_steps_skip, &
                                   plot_int, seed, stats_int, &
                                   bc_lo, bc_hi, probin_common_init, cfl, max_step, &
-                                  molmass, variance_coef_mass
+                                  molmass, variance_coef_mass, temp_bc_comp
   use probin_multispecies_module, only: nspecies, Dbar, rho_init, rho_bc, &
                                         mol_frac_bc_comp, print_error_norms, &
                                         rho_part_bc_comp, &
-                                        start_time, temp_bc_comp, timeinteg_type, &
+                                        start_time, timeinteg_type, &
                                         probin_multispecies_init
  
   implicit none
