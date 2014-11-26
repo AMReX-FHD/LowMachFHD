2b0cc4d8796be992ea457f6773ab7ca78fe10c76
ajnonaka <AJNonaka@lbl.gov>
2014-11-26 08:30:18 -0800

    add barodiffusion type 2 to overdamped alg

--- src_multiSpec/advance_timestep_overdamped.f90
+++ src_multiSpec/advance_timestep_overdamped.f90
@@ -167,6 +167,15 @@ contains
     ! compute grad p^{n-1/2}
     call compute_grad(mla,pres,gradp,dx,1,pres_bc_comp,1,1,the_bc_tower%bc_tower_array)
 
+    ! barodiffusion uses lagged pressure
+    if (barodiffusion_type .eq. 2) then
+       do n=1,nlevs
+          do i=1,dm
+             call multifab_copy_c(gradp_baro(n,i),1,gradp(n,i),1,1,0)
+          end do
+       end do
+    end if
+
     ! subtract grad p^{n-1/2} from gmres_rhs_v
     do n=1,nlevs
        do i=1,dm
@@ -429,6 +438,15 @@ contains
 
     ! compute grad p^*
     call compute_grad(mla,pres,gradp,dx,1,pres_bc_comp,1,1,the_bc_tower%bc_tower_array)
+
+    ! barodiffusion
+    if (barodiffusion_type .eq. 2) then
+       do n=1,nlevs
+          do i=1,dm
+             call multifab_copy_c(gradp_baro(n,i),1,gradp(n,i),1,1,0)
+          end do
+       end do
+    end if
 
     ! subtract grad p^* from gmres_rhs_v
     do n=1,nlevs
