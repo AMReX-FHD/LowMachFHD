1db3f4484479b5613ff8533ebb50fbc3d0492b0b
Andy Nonaka <AJNonaka@lbl.gov>
2015-03-16 14:51:03 -0700

    subroutine to convert rhoh to h and vice versa

--- src_multiSpec/convert_variables.f90
+++ src_multiSpec/convert_variables.f90
@@ -9,7 +9,7 @@ module convert_variables_module
 
   private
 
-  public :: convert_rho_to_c
+  public :: convert_rho_to_c, convert_rhoh_to_h
   
 contains
 
@@ -49,5 +49,38 @@ contains
     end if
 
   end subroutine convert_rho_to_c
+
+  subroutine convert_rhoh_to_h(mla,rhoh,rho_tot,h,rhoh_to_h)
+    
+    type(ml_layout), intent(in   ) :: mla
+    type(multifab) , intent(inout) :: rhoh(:)
+    type(multifab) , intent(in   ) :: rho_tot(:)
+    type(multifab) , intent(inout) :: h(:)
+    logical        , intent(in   ) :: rhoh_to_h
+
+    ! local
+    integer :: n,nlevs,i
+
+    nlevs = mla%nlevel
+
+    if (rhoh_to_h) then
+
+       ! rhoh to h - NO GHOST CELLS
+       do n=1,nlevs
+          call multifab_copy_c(h(n),1,rhoh(n),1,1,0)
+          call multifab_div_div_c(h(n),1,rho_tot(n),1,1,0)
+       end do
+
+    else
+
+       ! h to rhoh- VALID + GHOST (CAN CHANGE TO DO ONLY GHOST TO SAVE COMPUTATION)
+       do n=1,nlevs
+          call multifab_copy_c(rhoh(n),1,h(n),1,1,rhoh(n)%ng)
+          call multifab_mult_mult_c(rhoh(n),1,rho_tot(n),1,1,rhoh(n)%ng)
+       end do
+
+    end if
+
+  end subroutine convert_rhoh_to_h
 
 end module convert_variables_module
