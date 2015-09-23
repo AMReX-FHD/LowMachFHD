ef39fcee6ad080c86f3eb475699b246b912cd23e
Andy Nonaka <AJNonaka@lbl.gov>
2015-09-23 15:25:55 -0700

    implemented explicit midpoint scheme

--- src_reactDiff/advance_timestep.f90
+++ src_reactDiff/advance_timestep.f90
@@ -29,6 +29,7 @@ contains
     ! local
     type(multifab) :: diff_fluxdiv(mla%nlevel)
     type(multifab) :: stoch_fluxdiv(mla%nlevel)
+    type(multifab) :: stoch_fluxdiv2(mla%nlevel)
     type(multifab) :: diff_coef_face(mla%nlevel,mla%dim)
 
     integer :: n,nlevs,i,dm,comp
@@ -39,6 +40,7 @@ contains
     do n=1,nlevs
        call multifab_build(diff_fluxdiv(n) ,mla%la(n),nspecies,0) 
        call multifab_build(stoch_fluxdiv(n),mla%la(n),nspecies,0) 
+       call multifab_build(stoch_fluxdiv2(n),mla%la(n),nspecies,0) 
        do i=1,dm
           call multifab_build_edge(diff_coef_face(n,i),mla%la(n),nspecies,0,i)
        end do
@@ -68,8 +70,8 @@ contains
        ! explicit predictor-corrector
 
        ! Euler predictor
-       ! n_k^{n+1,*} = n_k^n + dt(div D_k grad n_k)^n
-       !                     + dt(div (sqrt(2 D_k n_k / dt) Z)^n
+       ! n_k^{n+1,*} = n_k^n + dt div (D_k grad n_k)^n
+       !                     + dt div (sqrt(2 D_k n_k / dt) Z)^n
        do n=1,nlevs
           call multifab_copy_c(n_new(n),1,n_old(n),1,nspecies,0)
           call multifab_saxpy_3(n_new(n),dt,diff_fluxdiv(n))
@@ -79,13 +81,13 @@ contains
                                the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
        end do
 
-       ! compute diffusive flux diverge
+       ! compute diffusive flux divergence
        call diffusive_n_fluxdiv(mla,n_new,diff_coef_face,diff_fluxdiv,dx,the_bc_tower)
 
        ! Trapezoidal corrector:
-       ! n_k^{n+1} = n_k^n + (dt/2)(div D_k grad n_k)^n
-       !                   + (dt/2)(div D_k grad n_k)^{n+1,*}
-       !                   + (dt)(div (sqrt(2 D_k n_k / dt) Z)^n
+       ! n_k^{n+1} = n_k^n + (dt/2) div (D_k grad n_k)^n
+       !                   + (dt/2) div (D_k grad n_k)^{n+1,*}
+       !                   +  dt    div (sqrt(2 D_k n_k / dt) Z)^n
        do n=1,nlevs
           call multifab_plus_plus_c(n_new(n),1,n_old(n),1,nspecies,0)
           call multifab_saxpy_3(n_new(n),dt,diff_fluxdiv(n))
@@ -97,17 +99,58 @@ contains
        end do
 
     else if (algorithm_type .eq. 1) then
-
+       ! Crank-Nicolson
+       ! n_k^{n+1} = n_k^n + (dt/2) div )D_k grad n_k)^n
+       !                   + (dt/2) div (D_k grad n_k)^n+1
+       !                   +  dt    div (sqrt(2 D_k n_k / dt) Z)^n
        call implicit_diffusion(mla,n_old,n_new,diff_coef_face,diff_fluxdiv,stoch_fluxdiv, &
                                dx,dt,the_bc_tower)
 
+    else if (algorithm_type .eq. 2) then
+       ! explicit midpoint scheme
+
+       ! n_k^{n+1/2} = n_k^n + (dt/2) div (D_k grad n_k)^n
+       !                     +  dt    div (sqrt(D_k n_k / dt) Z_1)^n
+       do n=1,nlevs
+          call multifab_copy_c(n_new(n),1,n_old(n),1,nspecies,0)
+          call multifab_saxpy_3(n_new(n),dt/2.d0      ,diff_fluxdiv(n))
+          call multifab_saxpy_3(n_new(n),dt/sqrt(2.d0),stoch_fluxdiv(n))
+          call multifab_fill_boundary(n_new(n))
+          call multifab_physbc(n_new(n),1,scal_bc_comp,nspecies, &
+                               the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
+       end do
+
+       ! compute diffusive flux divergence at t^{n+1/2}
+       call diffusive_n_fluxdiv(mla,n_new,diff_coef_face,diff_fluxdiv,dx,the_bc_tower)
+
+       ! fill random flux multifabs with new random numbers
+       call fill_mass_stochastic(mla,the_bc_tower%bc_tower_array)
+
+       ! compute second-stage stochastic flux divergence
+       call stochastic_n_fluxdiv(mla,n_old,diff_coef_face,stoch_fluxdiv2,dx,dt,the_bc_tower)
+
+       ! n_k^{n+1} = n_k^n + dt div (D_k grad n_k)^{n+1/2}
+       !                   + dt div (sqrt(D_k n_k / dt) Z_1)^n
+       !                   + dt div (sqrt(D_k n_k / dt) Z_2)^n
+       do n=1,nlevs
+          call multifab_copy_c(n_new(n),1,n_old(n),1,nspecies,0)
+          call multifab_saxpy_3(n_new(n),dt           ,diff_fluxdiv(n))
+          call multifab_saxpy_3(n_new(n),dt/sqrt(2.d0),stoch_fluxdiv(n))
+          call multifab_saxpy_3(n_new(n),dt/sqrt(2.d0),stoch_fluxdiv2(n))
+          call multifab_fill_boundary(n_new(n))
+          call multifab_physbc(n_new(n),1,scal_bc_comp,nspecies, &
+                               the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
+       end do
+
     else
+
        call bl_error("invalid algorithm_type")
     end if
 
     do n=1,nlevs
        call multifab_destroy(diff_fluxdiv(n))
        call multifab_destroy(stoch_fluxdiv(n))
+       call multifab_destroy(stoch_fluxdiv2(n))
        do i=1,dm
           call multifab_destroy(diff_coef_face(n,i))
        end do
