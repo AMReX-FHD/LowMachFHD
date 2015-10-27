a39782511922c187962c1e35ee8ffe898a064e3d
Andy Nonaka <AJNonaka@lbl.gov>
2015-10-27 14:07:53 -0700

    time-dependent bc case is now squirreled away in git lore

--- src_reactDiff/advance_timestep.f90
+++ src_reactDiff/advance_timestep.f90
@@ -29,11 +29,7 @@ contains
 
     type(multifab) :: ext_src_d(mla%nlevel)
     type(multifab) :: ext_src_r(mla%nlevel)
-    type(multifab) :: zerofab(mla%nlevel)
     type(multifab) :: z(mla%nlevel)
-
-    ! n_i boundary conditions (dir,lohi,species)
-    real(kind=dp_t) :: n_bc_temp(mla%dim,2,nspecies)
 
     type(bl_prof_timer),save :: bpt
 
@@ -77,98 +73,6 @@ contains
        call advance_diffusion(mla,n_old,n_new,ext_src_d,dx,0.5d0*dt,the_bc_tower) ! swap n_new/n_old to avoid calling copy()
 
     case(3)
-       ! (1/2)D + R + (1/2)D with inhomogeneous, time-dependent boundary conditions
-       ! under development
-
-       do n=1,nlevs
-          call multifab_build(z(n),mla%la(n),nspecies,0)
-          call multifab_build(zerofab(n),mla%la(n),nspecies,0)
-          call setval(zerofab(n),0.d0,all=.true.)
-       end do
-
-       ! compute z with new-time boundary conditions
-       !
-       !
-
-       do n=1,nlevs
-          ! temporary hack to test n_bc=1 case
-          ! in general we will solve div D_k grad z_k = 0
-          call setval(z(n),1.d0,all=.true.)
-       end do
-
-       ! save boundary conditions
-       n_bc_temp(1:dm,1:2,1:nspecies) = n_bc(1:dm,1:2,1:nspecies)
-
-       ! make boundary conditions homogeneous
-       n_bc(1:dm,1:2,1:nspecies) = 0.d0
-
-       ! store reactions rates for z in ext_src_d
-       ! store (negative) reaction rates for z in ext_src_r
-       ! the input time step does not matter as the reaction_type/use_Poisson_rng settings are
-       ! returning an explicit rate in units of number_density/time
-       call advance_reaction(mla,z,ext_src_d,zerofab,dx,dt,the_bc_tower,return_rates_in=.true.)
-       do n=1,nlevs
-          call multifab_copy_c(ext_src_r(n),1,ext_src_d(n),1,nspecies,0)
-          call multifab_mult_mult_s_c(ext_src_r(n),1,-1.d0,nspecies,0)
-       end do
-       
-       ! compute \tilde{n}
-       do n=1,nlevs
-          call multifab_sub_sub_c(n_old(n),1,z(n),1,nspecies,0)
-          call multifab_fill_boundary(n_old(n))
-          call multifab_physbc(n_old(n),1,scal_bc_comp,nspecies, &
-                               the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
-       end do
-
-       ! advance diffusion
-       call advance_diffusion(mla,n_old,n_new,ext_src_d,dx,0.5d0*dt,the_bc_tower)
-
-       do n=1,nlevs
-          call multifab_copy_c(n_old(n),1,n_new(n),1,nspecies,n_old(n)%ng)
-       end do
-
-       ! compute \tilde{n} + z
-       do n=1,nlevs
-          call multifab_plus_plus_c(n_old(n),1,z(n),1,nspecies,0)
-       end do
-
-       ! compute reaction rates, store in n_new
-       call advance_reaction(mla,n_old,n_new,ext_src_r,dx,dt,the_bc_tower,return_rates_in=.true.)
-
-       ! restore \tilde{n}
-       do n=1,nlevs
-          call multifab_sub_sub_c(n_old(n),1,z(n),1,nspecies,0)
-       end do
-
-       ! \tilde{n} = \tilde{n} + dt*reactions
-       do n=1,nlevs
-          call multifab_saxpy_3(n_old(n),dt,n_new(n))
-       end do
-
-       ! advance diffusion
-       call advance_diffusion(mla,n_old,n_new,ext_src_d,dx,0.5d0*dt,the_bc_tower)
-
-       ! restore boundary conditions
-       n_bc(1:dm,1:2,1:nspecies) = n_bc_temp(1:dm,1:2,1:nspecies)
-
-       ! (later) recompute z with new-time boundary conditions
-       !
-       !
-
-       ! n_new = n_new + z
-       do n=1,nlevs
-          call multifab_plus_plus_c(n_new(n),1,z(n),1,nspecies,0)
-          call multifab_fill_boundary(n_new(n))
-          call multifab_physbc(n_new(n),1,scal_bc_comp,nspecies, &
-                               the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
-       end do
-
-       do n=1,nlevs
-          call multifab_destroy(z(n))
-          call multifab_destroy(zerofab(n))
-       end do
-
-    case(4)
        ! (1/2)D + R + (1/2)D with inhomogeneous, time-independent boundary conditions
        ! under development
 
