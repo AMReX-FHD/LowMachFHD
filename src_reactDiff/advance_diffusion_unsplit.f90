3573bf9a26df2da0a1395c690396c1e9e4af6027
Changho Kim <changhokim@lbl.gov>
2015-12-07 19:25:51 -0800

    The unsplitting explicit-midpoint scheme has been implemented.

--- src_reactDiff/advance_diffusion_unsplit.f90
+++ src_reactDiff/advance_diffusion_unsplit.f90
@@ -34,12 +34,19 @@ contains
     type(multifab) :: diff_coef_face(mla%nlevel,mla%dim)
     type(multifab) :: ext_src(mla%nlevel)
     type(multifab) :: rate1(mla%nlevel)
-!!!    will need for midpoint
-!!!    type(multifab) :: rate2(mla%nlevel) 
+    type(multifab) :: rate2(mla%nlevel) 
 
     integer :: nlevs,dm,n,i,spec
 
     type(bl_prof_timer),save :: bpt
+
+    ! temporary 
+    ! (later, midpoint_stoch_flux_type will be included in probin_reactdiff.)
+    ! there are three options for updating the stochastic flux term of the corrector.
+    ! midpoint_stoch_flux_type = 1: K_n * W1 + K_n * W2
+    ! midpoint_stoch_flux_type = 2: K_n * W1 + K_pred * W2
+    ! midpoint_stoch_flux_type = 3: K_n * W1 + K_{2*pred-n} * W2
+    integer, parameter :: midpoint_stoch_flux_type = 1
 
     !!!!!!!!
     ! init !
@@ -64,6 +71,10 @@ contains
       end do
       call multifab_build(ext_src(n),mla%la(n),nspecies,0)
       call multifab_build(rate1(n),mla%la(n),nspecies,0)
+
+      if (diffusion_type .eq. 2) then ! explitcit midpoint
+        call multifab_build(rate2(n),mla%la(n),nspecies,0)
+      end if
     end do
 
     ! ext_src
@@ -127,6 +138,85 @@ contains
                              the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
       end do
 
+    else if (diffusion_type .eq. 2) then  ! explicit midpoint
+
+      ! calculate rates from a(n_old)
+      call advance_reaction(mla,n_old,rate1,dx,dt/2.d0,the_bc_tower,return_rates_in=2)
+
+      ! predictor
+      do n=1,nlevs
+        call multifab_copy_c(n_new(n),1,n_old(n),1,nspecies,0)
+        call multifab_saxpy_3(n_new(n),dt/2.d0,diff_fluxdiv(n))
+        call multifab_saxpy_3(n_new(n),dt/sqrt(2.d0),stoch_fluxdiv(n))
+        call multifab_saxpy_3(n_new(n),dt/2.d0,rate1(n))
+        call multifab_saxpy_3(n_new(n),dt/2.d0,ext_src(n))
+
+        call multifab_fill_boundary(n_new(n))
+        call multifab_physbc(n_new(n),1,scal_bc_comp,nspecies, &
+                             the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
+      end do
+
+      !!!!!!!!!!!!!!!
+      ! second step !
+      !!!!!!!!!!!!!!!
+
+      ! compute diffusive flux divergence
+      call diffusive_n_fluxdiv(mla,n_new,diff_coef_face,diff_fluxdiv,dx,the_bc_tower)
+
+      ! calculate rates from 2*a(n_pred)-a(n_old)
+      call advance_reaction(mla,n_old,rate2,dx,dt/2.d0,the_bc_tower,return_rates_in=2,n_pred_mattingly=n_new)
+
+      ! compute stochastic flux divergence
+      if (variance_coef_mass .gt. 0.d0) then
+
+        ! first, fill random flux multifabs with new random numbers
+        call fill_mass_stochastic(mla,the_bc_tower%bc_tower_array)
+
+        ! three possibilities
+        select case (midpoint_stoch_flux_type)
+        case (1)
+          ! use n_old
+          call stochastic_n_fluxdiv(mla,n_old,diff_coef_face,stoch_fluxdiv,dx,dt, &
+                                    the_bc_tower,increment_in=.true.)
+        case (2)
+          ! use n_pred 
+          call stochastic_n_fluxdiv(mla,n_new,diff_coef_face,stoch_fluxdiv,dx,dt, &
+                                    the_bc_tower,increment_in=.true.)
+        case (3)
+          ! compute 2*n_pred-n_old
+          do n=1,nlevs
+            call multifab_mult_mult_s_c(n_new(n),1,2.d0,nspecies,0)
+            call multifab_sub_sub_c(n_new(n),1,n_old(n),1,nspecies,0)
+          end do
+          ! use 2*n_pred-n_old
+          call stochastic_n_fluxdiv(mla,n_new,diff_coef_face,stoch_fluxdiv,dx,dt, &
+                                    the_bc_tower,increment_in=.true.)
+        case default
+          call bl_error("advance_diffusion_unsplit: invalid midpoint_stoch_flux_type")
+        end select
+
+      else
+
+        do n=1,nlevs
+          call multifab_setval(stoch_fluxdiv(n),0.d0,all=.true.)
+        end do
+
+      end if
+
+      ! update
+      do n=1,nlevs
+        call multifab_copy_c(n_new(n),1,n_old(n),1,nspecies,0)
+        call multifab_saxpy_3(n_new(n),dt,diff_fluxdiv(n))
+        call multifab_saxpy_3(n_new(n),dt/sqrt(2.d0),stoch_fluxdiv(n))
+        call multifab_saxpy_3(n_new(n),dt/2.d0,rate1(n))
+        call multifab_saxpy_3(n_new(n),dt/2.d0,rate2(n))
+        call multifab_saxpy_3(n_new(n),dt,ext_src(n))
+
+        call multifab_fill_boundary(n_new(n))
+        call multifab_physbc(n_new(n),1,scal_bc_comp,nspecies, &
+                             the_bc_tower%bc_tower_array(n),dx_in=dx(n,:))
+      end do
+
     else
       call bl_error("advance_diffusion_unsplit: invalid diffusion_type")
     end if
@@ -143,6 +233,10 @@ contains
       end do
       call multifab_destroy(ext_src(n))
       call multifab_destroy(rate1(n))
+
+      if (diffusion_type .eq. 2) then ! explicit midpoint
+        call multifab_destroy(rate2(n))
+      end if
     end do
 
     call destroy(bpt)
