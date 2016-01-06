643bffc99f8407d755d5426d6147d00120e5f778
Aleksandar Donev <donev@courant.nyu.edu>
2016-01-05 19:36:42 -0500

    Added MultinomialRNG to HydroGrid and updated hist_n.m to plot both Poisson and Stirling

--- exec/reactDiff/test/hist_n.m
+++ exec/reactDiff/test/hist_n.m
@@ -1,13 +1,13 @@
 clear all; format compact; format short
 
 u_av=1.0; % Average concentration
-dx=10.0; % Grid size (cell volume)
+dx=5.0; % Grid size (cell volume in higher dimensions)
 
 u_std = sqrt(u_av*dx)/dx; % Expected standard deviation
 
-u_std_int = nearest(sqrt(u_av*dx))/dx; % Ensure that the bins are centered on actual integers
+u_std_int = round(sqrt(u_av*dx))/dx; % Ensure that the bins are centered on actual integers
 range = [u_av - 4*u_std_int, u_av + 6*u_std_int];
-Nbins=nearest((range(2)-range(1))*dx)+1
+Nbins=round((range(2)-range(1))*dx)+1
 
 bins = linspace(range(1), range(2), Nbins);
 bins_th = linspace(range(1), range(2), 10000);
@@ -18,12 +18,18 @@ bounds=[bins(1), bins(end)]
 Gaussian = exp(-(bins_th-u_av).^2/(2*u_std.^2))/sqrt(2*pi)/u_std;
 
 posbins=bins_th(bins_th>0);
-Poisson=zeros(1,length(bins_th));
-Poisson(bins_th>0) = exp( -posbins.*(log(posbins/(u_av-1/(2*dx)))-1)*dx );
-Poisson = Poisson / (sum(Poisson(:))*du_th);
+Stirling=zeros(1,length(bins_th));
+Poisson=zeros(1,length(bins));
+% This is Stirling's approximation to the Poisson distribution:
+Stirling(bins_th>0) = exp( -posbins.*(log(posbins/(u_av-1/(2*dx)))-1)*dx );
+Stirling = Stirling / (sum(Stirling(:))*du_th);
+% True Poisson distribution
+bins_nonneg = bins(round(bins*dx)>=0);
+Poisson = poisspdf(round(bins_nonneg*dx), u_av*dx); % Use MATLAB's built-in function poisspdf to evaluate lambda^k*exp(-lambda)/k!
+Poisson = Poisson / (sum(Poisson)*du);
 
-compare=1 % Many curves at once or just a single run
-if(compare) % Compare different
+compare=0 % Many curves at once or just a single run
+if(compare) % Compare different curves
    
    f_data=load('/home/donev/HPC/FluctHydro/Chemistry/ReactDiff/Schlogl/fort.10.chem','-ascii'); f_data=f_data(:,2);
    u_hist_0=hist(f_data,bins); u_hist_0=u_hist_0/(sum(u_hist_0(:))*du);
@@ -34,7 +40,8 @@ if(compare) % Compare different
    
 else
 
-   f_data=load('/home/donev/HPC/FluctHydro/Chemistry/ReactDiff/Schlogl/fort.10','-ascii');
+   %f_data=load('/home/donev/HPC/FluctHydro/Chemistry/ReactDiff/Schlogl/fort.10','-ascii');
+   f_data=load('fort.10','-ascii');
    f_data=f_data(:,2); % Remove the time label
    u_hist=hist(f_data,bins);
    u_hist_std=sqrt(u_hist);
@@ -64,16 +71,17 @@ elseif(compare)
    legend('Chem only','Diff only','Chem+Diff','Gaussian','Poisson');  
 else
    semilogy(bins, u_hist, 'ok'); hold on;
-   semilogy(bins_th, Gaussian, '-r'); hold on;
-   semilogy(bins_th, Poisson, '-g'); hold on;
-   legend('Numerics','Gaussian','Poisson');
+   semilogy(bins_nonneg, Poisson, '-b'); hold on;
+   semilogy(bins_th, Stirling, '--g'); hold on;
+   semilogy(bins_th, Gaussian, '--r'); hold on;
+   legend('Numerics','Poisson','Stirling','Gaussian');
 end
-title('Monomodal Schlogl model N=10 particles per cell'); 
+title(['N=',int2str(round(u_av*dx)),' particles per cell']); 
 
 if(0) % Save the histograms to a file
-array=zeros(Nbins,3);
-array(:,1)=bins;
-array(:,2)=Poisson;
-array(:,3)=Gaussian;
-save('/home/donev/HPC/FluctHydro/Chemistry/ReactDiff/hist-n_25.dat','array','-ascii');
+   array=zeros(Nbins,3);
+   array(:,1)=bins;
+   array(:,2)=Poisson;
+   array(:,3)=Gaussian;
+   save('/home/donev/HPC/FluctHydro/Chemistry/ReactDiff/hist-n_25.dat','array','-ascii');
 end
