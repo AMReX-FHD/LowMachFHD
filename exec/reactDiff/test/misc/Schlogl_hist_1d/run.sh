daa8d71c9d5f0055af686a439aefb68f82ea8b3e
Changho Kim <changhokim@lbl.gov>
2016-01-21 11:54:10 -0800

    now "run.sh" can run multiple serial jobs concurrently by using mpiexec.

--- exec/reactDiff/test/misc/Schlogl_hist_1d/run.sh
+++ exec/reactDiff/test/misc/Schlogl_hist_1d/run.sh
@@ -5,20 +5,25 @@
 ###########
 
 RUNNAME=TEST
-NRUN=8
+NRUN=16
+USEMPIEXEC=yes
 
-dV=5.
+#dV=5.
+dV=100.
 
-OPT0="--nreactions 0"
-#OPT0="--nreactions 4 --rate_multiplier 0.1"
+OPT1="--nreactions 0"
+#OPT1="--nreactions 4 --rate_multiplier 0.1"
 
-OPT1="--fixed_dt 0.1 --max_step 50000 --print_int 10000 --hydro_grid_int 5 --n_steps_skip 10000"
+OPT2="--fixed_dt 0.1 --max_step 110000 --print_int 10000 --hydro_grid_int 5 --n_steps_skip 10000"
+#OPT2="--fixed_dt 0.01 --max_step 1100000 --print_int 100000 --hydro_grid_int 50 --n_steps_skip 100000"
 
-OPT2="--temporal_integrator -2"
-#OPT2="--temporal_integrator 0 --diffusion_type 3 --reaction_type 0"
+OPT3="--temporal_integrator -2 --avg_type 3 --midpoint_stoch_flux_type 2"
+#OPT3="--temporal_integrator -4 --avg_type 3 --midpoint_stoch_flux_type 2"
+#OPT3="--temporal_integrator 0 --diffusion_type 3 --reaction_type 0"
 
-OPT3="--use_Poisson_rng 1 --avg_type 3"
-OPT4="--midpoint_stoch_flux_type 2"
+#OPT4="--use_Poisson_rng 1"
+
+OPT5="--initial_variance 0."
 
 #######
 # RUN #
@@ -26,9 +31,10 @@ OPT4="--midpoint_stoch_flux_type 2"
 
 EXEC=../../main.Linux.gfortran.debug.mpi.exe
 INPUTS=inputs_Schlogl_hist_1d
-OPTS="--cross_section $dV $OPT0 $OPT1 $OPT2 $OPT3 $OPT4"
+OPTS="--cross_section $dV $OPT1 $OPT2 $OPT3 $OPT4 $OPT5"
+PYSCR_HIST=../../hist_n.py
 
-if [ $NRUN == 1 ]
+if [ "$NRUN" == 1 ]
 then
   if [ -d $RUNNAME ]; then echo "Warning: $RUNNAME already present"; else mkdir $RUNNAME; fi
 
@@ -38,8 +44,7 @@ then
   echo "../$EXEC ../$INPUTS $OPTS" | tee screen_out; sleep 3s
   ../$EXEC ../$INPUTS $OPTS | tee -a screen_out
 
-  PYSCR_HIST=../../hist_n.py
-  python ../$PYSCR_HIST fort.10 res.hist res.hist_cont res.hist_poiss 1. $dV
+  python ../$PYSCR_HIST fort.10 res.hist res.hist_cont res.hist_poiss 1. $dV yes
 else
   for ((i=1;i<=$NRUN;i++))
   do 
@@ -48,12 +53,33 @@ else
     echo "cd $RUNNAME$i"
     cd $RUNNAME$i
 
-    echo "../$EXEC ../$INPUTS $OPTS" | tee screen_out; sleep 3s
-    ../$EXEC ../$INPUTS $OPTS | tee -a screen_out
+    if [ "$USEMPIEXEC" == yes ]
+    then
+      echo "mpiexec -n 1 ../$EXEC ../$INPUTS $OPTS >> screen_out &" | tee screen_out
+      mpiexec -n 1 ../$EXEC ../$INPUTS $OPTS &>> screen_out &
+    else
+      echo "../$EXEC ../$INPUTS $OPTS" | tee screen_out
+      ../$EXEC ../$INPUTS $OPTS | tee -a screen_out
+    fi
 
-    PYSCR_HIST=../../hist_n.py
-    python ../$PYSCR_HIST fort.10 res.hist res.hist_cont res.hist_poiss 1. $dV no
-
+    echo "cd -"
     cd -
+    echo "" 
+    sleep 3s
+  done
+
+  if [ "$USEMPIEXEC" == yes ]
+  then 
+    echo "waiting for all runs to finish..."
+    wait
+    echo ""
+  fi
+
+  for ((i=1;i<=$NRUN;i++))
+  do
+    echo "cd $RUNNAME$i"
+    cd $RUNNAME$i
+    python ../$PYSCR_HIST fort.10 res.hist res.hist_cont res.hist_poiss 1. $dV no
+    cd - > /dev/null
   done
 fi
