019c8cad16f03c24f7a732cb2158f961badd6ff5
Changho Kim <changhokim@lbl.gov>
2016-02-16 17:52:24 -0800

    test/misc/Schlogl_hist_2d has been modified so that it is compatible with new hist_n.py.

--- exec/reactDiff/test/misc/Schlogl_hist_2d/run.sh
+++ exec/reactDiff/test/misc/Schlogl_hist_2d/run.sh
@@ -6,10 +6,7 @@
 
 RUNNAME=TEST
 
-#dV=5.
 dV=10.
-#dV=100.
-#dV=1000.
 
 #OPT1="--nreactions 0"
 OPT1="--nreactions 4 --rate_multiplier 0.1"
@@ -20,16 +17,13 @@ OPT2="--fixed_dt 0.5  --max_step 12000  --print_int 1000   --hydro_grid_int 2   
 #OPT2="--fixed_dt 0.25 --max_step 24000  --print_int 2000   --hydro_grid_int 4   --n_steps_skip 4000"
 #OPT2="--fixed_dt 0.1  --max_step 60000  --print_int 5000   --hydro_grid_int 10  --n_steps_skip 10000"
 
-#OPT3="--temporal_integrator -2 --avg_type 1 --midpoint_stoch_flux_type 3"
-OPT3="--temporal_integrator -4 --avg_type 1 --midpoint_stoch_flux_type 3"
-#OPT3="--temporal_integrator 0 --diffusion_type 3 --reaction_type 1"
+#OPT3="--temporal_integrator -2"
+OPT3="--temporal_integrator -4"
 
-OPT4="--use_Poisson_rng 1"
+OPT4="--avg_type 1 --midpoint_stoch_flux_type 3"
 
 OPT5="--initial_variance 1."
-#OPT5="--initial_variance 1.  --integer_populsations T"
-#OPT5="--initial_variance 0.  --integer_populations T"
-#OPT5="--initial_variance -1. --integer_populations F"
+#OPT5="--initial_variance -1."
 
 #######
 # RUN #
