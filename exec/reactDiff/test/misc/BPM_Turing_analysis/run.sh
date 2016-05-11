f89167ced9c1c16a28ad387e06f7920182fde27c
Changho Kim <changhokim@lbl.gov>
2016-05-10 17:21:27 -0700

    a few sets of paramter values (fixed_dt, max_step, print_int, hydro_grid_int, n_steps_write_avg) for run.sh three inputs files: RDMD, explicit midpoint, implicit midpoint

--- exec/reactDiff/test/misc/BPM_Turing_analysis/run.sh
+++ exec/reactDiff/test/misc/BPM_Turing_analysis/run.sh
@@ -3,6 +3,14 @@
 RUNNAME=TEST
 RUNNO=
 INPUTSFILE=inputs-256x256-RDME
+#INPUTSFILE=inputs-256x256-EXMID
+#INPUTSFILE=inputs-256x256-IMMID
+
+#OPT1="--fixed_dt 0.015  --max_step 680000 --print_int 600 --hydro_grid_int -60 --n_steps_write_avg -60"
+OPT1="--fixed_dt 0.03   --max_step 340000 --print_int 300 --hydro_grid_int -30 --n_steps_write_avg -30"
+#OPT1="--fixed_dt 0.06   --max_step 170000 --print_int 150 --hydro_grid_int -15 --n_steps_write_avg -15"
+#OPT1="--fixed_dt 0.09   --max_step 120000 --print_int 100 --hydro_grid_int -10 --n_steps_write_avg -10"
+#OPT1="--fixed_dt 0.15   --max_step  68000 --print_int  60 --hydro_grid_int  -6 --n_steps_write_avg  -6"
 
 EXEC=../../main.Linux.gfortran.debug.mpi.exe
 NPROC=4
@@ -24,8 +32,8 @@ cd $RUNNAME
 
 if [ "$RUNNO" = "" ]
 then
-  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE | tee screen_out
+  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 | tee screen_out
 else
-  echo "mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE > screen_out &"
-  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE > screen_out &
+  echo "mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 > screen_out &"
+  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 > screen_out &
 fi
