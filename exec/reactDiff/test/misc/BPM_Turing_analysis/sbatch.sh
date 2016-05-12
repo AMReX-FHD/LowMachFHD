672fa68405d38a76faca9f2521d59810f8cced17
Changho Kim <changhokim@lbl.gov>
2016-05-12 12:28:21 -0700

    wrong location of inputs file in the sbatch script

--- exec/reactDiff/test/misc/BPM_Turing_analysis/sbatch.sh
+++ exec/reactDiff/test/misc/BPM_Turing_analysis/sbatch.sh
@@ -15,9 +15,9 @@ NRUN=16
 INPUTSFILE=inputs-256x256-RDME
 #INPUTSFILE=inputs-256x256-EXMID
 #INPUTSFILE=inputs-256x256-IMMID
-EXEC=../../main.Linux.Intel.debug.mpi.exe
+EXEC=../../main.Linux.Intel.mpi.exe
 
-OPT1="--fixed_dt 0.03   --max_step 30000 --print_int 300 --hydro_grid_int -30 --n_steps_write_avg -30"
+OPT1="--fixed_dt 0.03   --max_step 3000 --print_int 300 --hydro_grid_int -30 --n_steps_write_avg -30"
 #OPT1="--fixed_dt 0.015  --max_step 680000 --print_int 600 --hydro_grid_int -60 --n_steps_write_avg -60"
 #OPT1="--fixed_dt 0.03   --max_step 340000 --print_int 300 --hydro_grid_int -30 --n_steps_write_avg -30"
 #OPT1="--fixed_dt 0.06   --max_step 170000 --print_int 150 --hydro_grid_int -15 --n_steps_write_avg -15"
@@ -31,7 +31,7 @@ for ((i=1;i<=NRUN;i++))
 do
   mkdir ${RUNNAME}_RUN$i
   cd ${RUNNAME}_RUN$i
-  srun -n 4 ../../$EXEC $INPUTSFILE $OPT1 > screen_out &
+  srun -n 4 ../../$EXEC ../../$INPUTSFILE $OPT1 > screen_out &
   sleep 5s
   cd ..
 done
