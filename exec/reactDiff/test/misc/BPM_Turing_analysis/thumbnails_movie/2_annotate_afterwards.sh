097ffd31b83cf724f5a8be94ae437c428930ad65
Changho Kim <changhokim@lbl.gov>
2016-05-31 22:52:55 -0700

    settings of thumbnails_movie for BPM_Turing_analysis has been changed so that a new column for 64x64 resolution results is included. (that is, 5x3 thumbnails)

--- exec/reactDiff/test/misc/BPM_Turing_analysis/thumbnails_movie/2_annotate_afterwards.sh
+++ exec/reactDiff/test/misc/BPM_Turing_analysis/thumbnails_movie/2_annotate_afterwards.sh
@@ -6,14 +6,19 @@ DESTGIF=2.gif
 convert $SRCGIF -pointsize 10 \
   -annotate +20+35 "Particle 256x256 dt=0.05" \
   -annotate +168+35 "Particle 128x128 dt=0.2" \
+  -annotate +316+35 "Particle 64x64 dt=0.5" \
   -annotate +20+183 "RDME 256x256 dt=0.05" \
   -annotate +168+183 "RDME 128x128 dt=0.2" \
+  -annotate +316+183 "RDME 64x64 dt=0.5" \
   -annotate +20+331 "ExMid 256x256 dt=0.025" \
   -annotate +168+331 "ExMid 128x128 dt=0.1" \
+  -annotate +316+331 "ExMid 64x64 dt=0.25" \
   -annotate +20+479 "ImMid 256x256 dt=0.25" \
   -annotate +168+479 "ImMid 128x128 dt=0.5" \
+  -annotate +316+479 "ImMid 64x64 dt=0.5" \
   -annotate +20+627 "ImMid 256x256 dt=0.5" \
   -annotate +168+627 "ImMid 128x128 dt=1" \
-  -pointsize 15 -annotate +50+15 "Number Density of Species W" \
+  -annotate +316+627 "ImMid 64x64 dt=1" \
+  -pointsize 15 -annotate +132+15 "Number Density of Species U" \
   $DESTGIF
 eog $DESTGIF &
--- exec/reactDiff/test/misc/BPM_Turing_analysis/thumbnails_movie/input_thumbnails_info.py
+++ exec/reactDiff/test/misc/BPM_Turing_analysis/thumbnails_movie/input_thumbnails_info.py
@@ -4,15 +4,15 @@
 
 # numbers of rows and columns  (R=row,C=column)
 nR = 5
-nC = 2
+nC = 3
 
 # size of plots and margins (H=horizontal,V=vertical)
 # H=horizontal, 2*nC+1 components, from left to right
 # (margin,plot,margin,plot, ... ,margin,plot,margin)
-sizeH = (20,128,20,128,20)	
+sizeH = (20,128,20,128,20,128,20)
 # V=vertical, 2*nR+1 components, from bottom to top
 # (margin,plot,margin,plot, ... ,margin,plot,margin)
-sizeV = (50,128,20,128,20,128,20,128,20,128,40)     
+sizeV = (50,128,20,128,20,128,20,128,20,128,40)
 
 # plot number examples:
 # 	(1x2) 	(2x1)	(2x2)	(2x3) 	(3x2)
@@ -23,38 +23,53 @@ sizeV = (50,128,20,128,20,128,20,128,20,128,40)
 # databaseName, variableName, valMin, valMax, nTick, visible (color box)
 listData = []
 
-#listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n1",600,2000,3,False]	# plot1
-#listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n1",600,2000,3,True]	# plot2
-#listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n1",600,2000,3,False]	# plot3
-#listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n1",600,2000,3,False]	# plot4
-#listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n1",600,2000,3,False]	# plot5
-#listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n1",600,2000,3,False]	# plot6
-#listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n1",600,2000,3,False]	# plot7
-#listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n1",600,2000,3,False]	# plot8
-#listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar1",600,2000,3,False]	# plot9
-#listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar1",600,2000,3,False]	# plot10
+listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n1",600,2000,3,False]		# plot1
+listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n1",600,2000,3,False]		# plot2
+listData += ["../../64x64_ImMid_dt1/movie.visit","n1",600,2000,3,True]				# plot3
+listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n1",600,2000,3,False]		# plot4
+listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n1",600,2000,3,False]		# plot5
+listData += ["../../64x64_ImMid_dt0.5/movie.visit","n1",600,2000,3,False]			# plot6
+listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n1",600,2000,3,False]		# plot7
+listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n1",600,2000,3,False]		# plot8
+listData += ["../../64x64_ExMid_dt0.25/movie.visit","n1",600,2000,3,False]			# plot9
+listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n1",600,2000,3,False]		# plot10
+listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n1",600,2000,3,False]		# plot11
+listData += ["../../64x64_RDME_dt0.5/movie.visit","n1",600,2000,3,False]			# plot12
+listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar1",600,2000,3,False]	# plot13
+listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar1",600,2000,3,False]	# plot14
+listData += ["../../64x64_Particle_dt0.5/movie.visit","Scalar1",600,2000,3,False]		# plot15
 
-#listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n3",0,300,3,False]	# plot1
-#listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n3",0,300,3,True]		# plot2
-#listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n3",0,300,3,False]	# plot3
-#listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n3",0,300,3,False]	# plot4
-#listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n3",0,300,3,False]	# plot5
-#listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n3",0,300,3,False]	# plot6
-#listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n3",0,300,3,False]	# plot7
-#listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n3",0,300,3,False]	# plot8
-#listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar3",0,300,3,False]	# plot9
-#listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar3",0,300,3,False]	# plot10
+#listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n3",0,300,3,False]		# plot1
+#listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n3",0,300,3,False]			# plot2
+#listData += ["../../64x64_ImMid_dt1/movie.visit","n3",0,300,3,True]				# plot3
+#listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n3",0,300,3,False]		# plot4
+#listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n3",0,300,3,False]		# plot5
+#listData += ["../../64x64_ImMid_dt0.5/movie.visit","n3",0,300,3,False]				# plot6
+#listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n3",0,300,3,False]		# plot7
+#listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n3",0,300,3,False]		# plot8
+#listData += ["../../64x64_ExMid_dt0.25/movie.visit","n3",0,300,3,False]			# plot9
+#listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n3",0,300,3,False]		# plot10
+#listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n3",0,300,3,False]		# plot11
+#listData += ["../../64x64_RDME_dt0.5/movie.visit","n3",0,300,3,False]				# plot12
+#listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar3",0,300,3,False]	# plot13
+#listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar3",0,300,3,False]	# plot14
+#listData += ["../../64x64_Particle_dt0.5/movie.visit","Scalar3",0,300,3,False]			# plot15
 
-listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n3",-10,0,3,False]	# plot1
-listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n3",-10,0,3,True]		# plot2
-listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n3",-10,0,3,False]	# plot3
-listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n3",-10,0,3,False]	# plot4
-listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n3",-10,0,3,False]	# plot5
-listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n3",-10,0,3,False]	# plot6
-listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n3",-10,0,3,False]	# plot7
-listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n3",-10,0,3,False]	# plot8
-listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar3",-10,0,3,False]	# plot9
-listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar3",-10,0,3,False]	# plot10
+#listData += ["../../256x256_IMMID_dt0.5_factor4/movie.visit","n3",-10,0,3,False]		# plot1
+#listData += ["../../128x128_IMMID_dt1_factor2/movie.visit","n3",-10,0,3,False]			# plot2
+#listData += ["../../64x64_ImMid_dt1/movie.visit","n3",-10,0,3,True]				# plot3
+#listData += ["../../256x256_IMMID_dt0.25_factor4/movie.visit","n3",-10,0,3,False]		# plot4
+#listData += ["../../128x128_IMMID_dt0.5_factor2/movie.visit","n3",-10,0,3,False]		# plot5
+#listData += ["../../64x64_ImMid_dt0.5/movie.visit","n3",-10,0,3,False]				# plot6
+#listData += ["../../256x256_EXMID_dt0.025_factor4/movie.visit","n3",-10,0,3,False]		# plot7
+#listData += ["../../128x128_EXMID_dt0.1_factor2/movie.visit","n3",-10,0,3,False]		# plot8
+#listData += ["../../64x64_ExMid_dt0.25/movie.visit","n3",-10,0,3,False]			# plot9
+#listData += ["../../256x256_RDME_dt0.05_factor4/movie.visit","n3",-10,0,3,False]		# plot10
+#listData += ["../../128x128_RDME_dt0.2_factor2/movie.visit","n3",-10,0,3,False]		# plot11
+#listData += ["../../64x64_RDME_dt0.5/movie.visit","n3",-10,0,3,False]				# plot12
+#listData += ["../../256x256_Particle_dt0.05_factor4/movie.visit","Scalar3",-10,0,3,False]	# plot13
+#listData += ["../../128x128_Particle_dt0.2_factor2/movie.visit","Scalar3",-10,0,3,False]	# plot14
+#listData += ["../../64x64_Particle_dt0.5/movie.visit","Scalar3",-10,0,3,False]			# plot15
 
 # domain range
 domain_xlo = 0
@@ -63,7 +78,7 @@ domain_ylo = 0
 domain_yhi = 32
 
 # including text "Time=$time" for each plot
-listIncludeTimeText = [False,True,False,False,False,False,False,False,False,False]
+listIncludeTimeText = [False,False,True,False,False,False,False,False,False,False,False,False,False,False,False]
 
 # some parameters for color box legend
 param_cb1 = -0.07	# (-) left (+) right w.r.t. left-side of plot
