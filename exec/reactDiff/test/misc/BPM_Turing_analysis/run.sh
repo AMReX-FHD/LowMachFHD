25dcf37eab2eeb29724175ff6497ce25c6c41e0b
Changho Kim <changhokim@lbl.gov>
2016-05-31 22:37:58 -0700

    add more options to BPM_Turing_analysis/run.sh change inputs files accordingly

--- exec/reactDiff/test/misc/BPM_Turing_analysis/run.sh
+++ exec/reactDiff/test/misc/BPM_Turing_analysis/run.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-RUNNAME=TEST
+RUNNAME=64x64_ExMid_dt0.25
 RUNNO=
-INPUTSFILE=inputs-256x256-RDME
-#INPUTSFILE=inputs-256x256-EXMID
-#INPUTSFILE=inputs-256x256-IMMID
 
-#OPT1="--fixed_dt 0.025 --max_step 400000  --print_int 400  --hydro_grid_int 0  --n_steps_write_avg 40  --plot_int 0"
-OPT1="--fixed_dt 0.05  --max_step 200000  --print_int 200  --hydro_grid_int 0  --n_steps_write_avg 20  --plot_int 0"
-#OPT1="--fixed_dt 0.1   --max_step 100000  --print_int 100  --hydro_grid_int 0  --n_steps_write_avg 10  --plot_int 0"
-#OPT1="--fixed_dt 0.2   --max_step  50000  --print_int  50  --hydro_grid_int 0  --n_steps_write_avg  5  --plot_int 0"
-#OPT1="--fixed_dt 0.25  --max_step  40000  --print_int  40  --hydro_grid_int 0  --n_steps_write_avg  4  --plot_int 0"
-#OPT1="--fixed_dt 0.5   --max_step  20000  --print_int  20  --hydro_grid_int 0  --n_steps_write_avg  2  --plot_int 0"
-#OPT1="--fixed_dt 1.    --max_step  20000  --print_int  10  --hydro_grid_int 0  --n_steps_write_avg  1  --plot_int 0"
+### method ###
+#INPUTSFILE=inputs_RDME
+INPUTSFILE=inputs_ExMid
+#INPUTSFILE=inputs_ImMid
 
-EXEC=../../main.Linux.gfortran.debug.mpi.exe
+### resolution ###
+OPT1="--n_cells_x 64  --n_cells_y 64  --max_grid_size_x 32 --max_grid_size_y 32"
+#OPT1="--n_cells_x 128 --n_cells_y 128 --max_grid_size_x 32 --max_grid_size_y 32"
+#OPT1="--n_cells_x 256 --n_cells_y 256 --max_grid_size_x 32 --max_grid_size_y 32"
+
+### time step ###
+# for movie
+#OPT2="--fixed_dt 0.025 --max_step 400000  --print_int 400  --hydro_grid_int 0  --n_steps_write_avg 40  --plot_int 4000"
+#OPT2="--fixed_dt 0.05  --max_step 200000  --print_int 200  --hydro_grid_int 0  --n_steps_write_avg 20  --plot_int 2000"
+#OPT2="--fixed_dt 0.1   --max_step 100000  --print_int 100  --hydro_grid_int 0  --n_steps_write_avg 10  --plot_int 1000"
+#OPT2="--fixed_dt 0.2   --max_step  50000  --print_int  50  --hydro_grid_int 0  --n_steps_write_avg  5  --plot_int  500"
+OPT2="--fixed_dt 0.25  --max_step  40000  --print_int  40  --hydro_grid_int 0  --n_steps_write_avg  4  --plot_int  400"
+#OPT2="--fixed_dt 0.5   --max_step  20000  --print_int  20  --hydro_grid_int 0  --n_steps_write_avg  2  --plot_int  200"
+#OPT2="--fixed_dt 1.    --max_step  10000  --print_int  10  --hydro_grid_int 0  --n_steps_write_avg  1  --plot_int  100"
+# for quantitative analysis
+#OPT2="--fixed_dt 0.025 --max_step 300000  --print_int 400  --hydro_grid_int 0  --n_steps_write_avg 40  --plot_int 0"
+#OPT2="--fixed_dt 0.05  --max_step 150000  --print_int 200  --hydro_grid_int 0  --n_steps_write_avg 20  --plot_int 0"
+#OPT2="--fixed_dt 0.1   --max_step  75000  --print_int 100  --hydro_grid_int 0  --n_steps_write_avg 10  --plot_int 0"
+#OPT2="--fixed_dt 0.2   --max_step  37500  --print_int  50  --hydro_grid_int 0  --n_steps_write_avg  5  --plot_int 0"
+#OPT2="--fixed_dt 0.25  --max_step  30000  --print_int  40  --hydro_grid_int 0  --n_steps_write_avg  4  --plot_int 0"
+#OPT2="--fixed_dt 0.5   --max_step  15000  --print_int  20  --hydro_grid_int 0  --n_steps_write_avg  2  --plot_int 0"
+#OPT2="--fixed_dt 1.    --max_step   7500  --print_int  10  --hydro_grid_int 0  --n_steps_write_avg  1  --plot_int 0"
+
+### cross_section ###
+#OPT3="--cross_section 0.5"
+OPT3="--cross_section 1."
+#OPT3="--cross_section 2."
+
+OPTS="$OPT1 $OPT2 $OPT3"
+EXEC=../../main.Linux.gfortran.mpi.exe
 NPROC=4
 
 if [ "$RUNNO" != "" ]
@@ -30,11 +53,12 @@ else
   mkdir $RUNNAME
 fi
 
+cp run.sh $RUNNAME
 cd $RUNNAME
 
 if [ "$RUNNO" = "" ]
 then
-  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 | tee screen_out
+  mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPTS | tee screen_out
 else
   echo "mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 > screen_out &"
   mpiexec -np $NPROC ../$EXEC ../$INPUTSFILE $OPT1 > screen_out &
