7a3a04168667d1bba9b8809063e9a1193a21fe6c
Andy Nonaka <AJNonaka@lbl.gov>
2016-07-21 14:47:37 -0700

    beginning of RNG test program

---
+++ RNG_test/main.f90
@@ -0,0 +1,49 @@
+program main
+
+  use BoxLib
+  use BoxLibRNGs
+  use bl_random_module
+
+  implicit none
+
+  integer, parameter :: dp=kind(0.0d0), sp=kind(0.0)
+
+  type(bl_rng_engine) :: rng_eng
+
+  real(dp) :: real_dp
+  real(sp) :: real_sp
+
+  integer :: seed
+  integer :: i
+
+  call boxlib_initialize()
+
+  seed = 1
+
+  call bl_rng_build_engine(rng_eng,seed)
+
+  do i=1,10
+     call UniformRNG(real_dp,rng_eng%p)
+     print*,'BL UniformRNG with dp',real_dp
+  end do
+
+  do i=1,10
+     call UniformRNG(real_dp)
+     print*,'HG UniformRNG with dp',real_dp
+  end do
+
+  do i=1,10
+     call UniformRNG(real_sp,rng_eng%p)
+     print*,'BL UniformRNG with sp',real_sp
+  end do
+
+  do i=1,10
+     call UniformRNG(real_sp)
+     print*,'HG UniformRNG with sp',real_sp
+  end do
+
+  call bl_rng_destroy_engine(rng_eng)
+
+  call boxlib_finalize()
+
+end program main
