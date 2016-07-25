1db1e272d958d5850821b6277a65a38a26f61a28
Andy Nonaka <AJNonaka@lbl.gov>
2016-07-25 16:47:55 -0700

    code cleanup - Lorentz force now stores the actual force, not 1/2 the force

--- src_charged/advance_timestep_inertial.f90
+++ src_charged/advance_timestep_inertial.f90
@@ -398,23 +398,21 @@ contains
 
     if (use_charged_fluid) then
 
-       ! compute (1/2) old and new momentum charge force
+       ! compute old and new momentum charge force
        call average_cc_to_face(nlevs,charge_old,Lorentz_force_old,1,scal_bc_comp,1,the_bc_tower%bc_tower_array)
        call average_cc_to_face(nlevs,charge_new,Lorentz_force_new,1,scal_bc_comp,1,the_bc_tower%bc_tower_array)
        do n=1,nlevs
           do i=1,dm
              call multifab_mult_mult_c(Lorentz_force_old(n,i),1,grad_Epot_old(n,i),1,1,0)
              call multifab_mult_mult_c(Lorentz_force_new(n,i),1,grad_Epot_new(n,i),1,1,0)
-             call multifab_mult_mult_s_c(Lorentz_force_old(n,i),1,0.5d0,1,0)
-             call multifab_mult_mult_s_c(Lorentz_force_new(n,i),1,0.5d0,1,0)
           end do
        end do
 
        ! subtract (1/2) old and (1/2) new from gmres_rhs_v
        do n=1,nlevs
           do i=1,dm
-             call multifab_sub_sub_c(gmres_rhs_v(n,i),1,Lorentz_force_old(n,i),1,1,0)
-             call multifab_sub_sub_c(gmres_rhs_v(n,i),1,Lorentz_force_new(n,i),1,1,0)
+             call multifab_saxpy_3(gmres_rhs_v(n,i),-0.5d0,Lorentz_force_old(n,i))
+             call multifab_saxpy_3(gmres_rhs_v(n,i),-0.5d0,Lorentz_force_new(n,i))
           end do
        end do
 
@@ -805,15 +803,14 @@ contains
        do n=1,nlevs
           do i=1,dm
              call multifab_mult_mult_c(Lorentz_force_new(n,i),1,grad_Epot_new(n,i),1,1,0)
-             call multifab_mult_mult_s_c(Lorentz_force_new(n,i),1,0.5d0,1,0)
           end do
        end do
 
        ! subtract (1/2) old and (1/2) new from gmres_rhs_v
        do n=1,nlevs
           do i=1,dm
-             call multifab_sub_sub_c(gmres_rhs_v(n,i),1,Lorentz_force_old(n,i),1,1,0)
-             call multifab_sub_sub_c(gmres_rhs_v(n,i),1,Lorentz_force_new(n,i),1,1,0)
+             call multifab_saxpy_3(gmres_rhs_v(n,i),-0.5d0,Lorentz_force_old(n,i))
+             call multifab_saxpy_3(gmres_rhs_v(n,i),-0.5d0,Lorentz_force_new(n,i))
           end do
        end do
 
