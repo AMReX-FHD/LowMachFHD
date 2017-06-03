b5984ad82d9669c10064680c1edadbef6c1f0c61
Andy Nonaka <AJNonaka@lbl.gov>
2017-06-02 18:28:00 -0700

    comments only

--- src_lowMach/main_driver.f90
+++ src_lowMach/main_driver.f90
@@ -310,7 +310,7 @@ subroutine main_driver()
 
   if (restart .lt. 0) then
 
-     ! initialize rho
+     ! initialize rho and umac
      call init_rho_and_umac(mla,rho_old,umac,dx,time,the_bc_tower%bc_tower_array)
 
      ! initialize pi
