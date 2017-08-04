4fcd10d314baa7ad4a9f7eb5dabc936056376507
Changho Kim <changhokim@lbl.gov>
2017-08-04 14:45:08 -0700

    3spec equil S(k) test: scripts updated

--- exec/multiSpecLM_implicit/test/inputs_files/test_3spec_equil_fluct_2d/run.sh
+++ exec/multiSpecLM_implicit/test/inputs_files/test_3spec_equil_fluct_2d/run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 EXEC=../../main.Linux.gfortran.mpi.exe
-INPUTS=../inputs_paper_3spec_equil_fluct_2d
-RUNNAME=alg0_short
+INPUTS=inputs_3spec_equil_fluct_2d
+RUNNAME=alg6_short
 NPROC=4
 
 # algorithm type
-OPT1="--algorithm_type 0"               # inertial trapezoidal
+OPT1="--algorithm_type 6"              # boussinesq
+#OPT1="--algorithm_type 0"              # inertial trapezoidal
 #OPT1="--algorithm_type 2"              # overdamped
 #OPT1="--algorithm_type 5"              # inertial midpoint
 
 # smaller time average
+#OPT2="--max_step 10  --print_int 10  --n_steps_skip 0 --hydro_grid_int 0 --gmres_verbose 1"
 OPT2="--max_step 12000  --print_int 100  --n_steps_skip 2000"
 #OPT2="--max_step 120000 --print_int 1000 --n_steps_skip 20000"
 
-OPTS="$OPT1 $OPT2"
+# turn off gmres solver (velocity becomes exactly zero)
+#OPT3="--variance_coef_mom 0. --initial_variance 0. --gmres_abs_tol 1.e-9 --mg_abs_tol 1.e-9"
+
+OPTS="$OPT1 $OPT2 $OPT3"
 
 #######
 # RUN #
