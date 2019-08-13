aa9d3966e294469ff129a2d32f9d36527bcdf4c6
Andy Nonaka <AJNonaka@lbl.gov>
2019-08-12 18:26:59 -0700

    wrote a utility (2d only for now; 3d case generalizes easily) that takes in a multifab and writes out a planar slice at the lo coordinate orthogonal to the input directional coordinate.  Works for any specified component.  can be generalized later to write to files, etc.

--- staggered_grid/src_common/utility.f90
+++ staggered_grid/src_common/utility.f90
@@ -7,7 +7,7 @@ module utility_module
 
   private
 
-  public :: invert_multifab
+  public :: invert_multifab, planar_cut
 
 contains
 
@@ -48,5 +48,99 @@ contains
     call destroy(bpt)
 
   end subroutine invert_multifab
+
+  ! takes a planar cut of a staggered multifab in the dir direction
+  ! at the lo (index 0) side of the domain
+  subroutine planar_cut(mla,mf,comp,dir)
+
+    use probin_common_module, only: n_cells
+
+    type(ml_layout), intent(in   ) :: mla
+    type(multifab) , intent(in   ) :: mf(:)
+    integer        , intent(in   ) :: comp, dir
+
+    integer :: dm
+
+    dm = mla%dim
+
+    if (dm .eq. 2) then
+       call planar_cut_2d(mla,mf,comp,dir)
+    else if (dm .eq. 3) then
+       call bl_error("planar_cut_3d not written yet")
+    end if
+
+    contains
+
+      subroutine planar_cut_2d(mla,mf,comp,dir)
+
+        type(ml_layout), intent(in   ) :: mla
+        type(multifab) , intent(in   ) :: mf(:)
+        integer        , intent(in   ) :: comp
+        integer        , intent(in   ) :: dir
+
+        ! local
+        integer :: lo(mla%dim),hi(mla%dim)
+
+        real(kind=dp_t), allocatable :: cut(:)
+        real(kind=dp_t), allocatable :: cut_proc(:)
+        real(kind=dp_t), pointer :: mp(:,:,:,:)
+
+        integer :: i,ng
+
+        if (dir .eq. 1) then
+           allocate(cut     (0:n_cells(2)-1))
+           allocate(cut_proc(0:n_cells(2)-1))
+        else if (dir .eq. 2) then
+           allocate(cut     (0:n_cells(1)-1))
+           allocate(cut_proc(0:n_cells(1)-1))
+        end if
+
+        cut(:) = 0.d0
+        cut_proc(:) = 0.d0
+
+        ng = mf(1)%ng
+
+        do i=1,nfabs(mf(1))
+           mp => dataptr(mf(1),i)
+           lo = lwb(get_box(mf(1),i))
+           hi = upb(get_box(mf(1),i))
+           call planar_cut_fill_2d(lo,hi,mp(:,:,1,comp),ng,dir,cut_proc)
+        end do
+
+        call parallel_reduce(cut,cut_proc,MPI_SUM)
+
+        do i=0,size(cut)-1
+           if (parallel_IOProcessor()) then
+              print*,i,cut(i)
+           end if
+        end do
+
+      end subroutine planar_cut_2d
+
+      subroutine planar_cut_fill_2d(lo,hi,mf,ng,dir,cut)
+
+        integer        , intent(in   ) :: lo(:),hi(:),ng,dir
+        real(kind=dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+        real(kind=dp_t), intent(inout) :: cut(0:)
+
+        integer :: i,j
+
+        if (dir .eq. 1) then
+           if (lo(1) .eq. 0) then
+              do j=lo(2),hi(2)
+                 cut(j) = mf(0,j)
+              end do
+           end if
+        else if (dir .eq. 2) then
+           if (lo(2) .eq. 0) then
+              do i=lo(1),hi(1)
+                 cut(i) = mf(i,0)
+              end do
+           end if
+        end if
+
+      end subroutine planar_cut_fill_2d
+
+    end subroutine planar_cut
 
 end module utility_module
