3eb8779ec10319be11314dac7cf09f01bd06b0ea
Aleksandar Donev <donev@courant.nyu.edu>
2019-08-14 14:51:36 -0400

    Working on adding analysis code for planar cuts

---
+++ staggered_grid/src_common/user_analysis.f90
@@ -0,0 +1,43 @@
+module user_analysis
+  ! This module contains user-specific routines for analyzing data
+  ! Right not this only analyses fluxes on a planar cut through the domain
+
+  use bl_types
+  use bl_error_module 
+  use probin_common
+  implicit none
+
+  private
+
+  public :: analyze_planar_cut
+
+contains
+
+  subroutine initialize_planar_cut()
+    call bl_warn("using default initialize_planar_cut")
+    call bl_warn("each application code should have its own copy")
+  
+  end subroutine
+
+  subroutine analyze_planar_cut(cut)
+    real(kind=dp_t), allocatable :: cut(:,:) ! Cut through a plane
+
+    call bl_warn("using default analyze_planar_cut; printing average values to screen")
+    call bl_warn("each application code should have its own copy")
+
+    if (parallel_IOProcessor()) then      
+      do i=1,size(cut,1)
+         write(*,*) cut(i,:)
+      end do
+    end if
+
+  end subroutine analyze_planar_cut
+
+  subroutine destroy_planar_cut()
+  
+    call bl_warn("using default destroy_planar_cut")
+    call bl_warn("each application code should have its own copy")
+  
+  end subroutine
+
+end module user_analysis
